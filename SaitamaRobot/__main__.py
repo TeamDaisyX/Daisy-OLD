d6de72a97b2d22825ff8ce36732ed60fc6a74295
Godzilla0userbot <72538870+Godzilla0userbot@users.noreply.github.com>
2020-10-20 01:00:23 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -85,7 +85,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/88b32c4e3613f3525a411.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/baf58ec4aca2df39c8b61.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
