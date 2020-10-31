fcf0091c56e850440a01728a944e55ace6fa9cd5
Godzilla0userbot <72538870+Godzilla0userbot@users.noreply.github.com>
2020-10-31 16:29:33 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -334,7 +334,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("SaitamaRobot{}.backup".format(chat_id), "rb"),
+        document=open("Suzuya_ProBot{}.backup".format(chat_id), "rb"),
         caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Suzuya_ProBot-Backup` was specially made for notes."
         .format(chat.title, chat_id, tgl),
         timeout=360,
