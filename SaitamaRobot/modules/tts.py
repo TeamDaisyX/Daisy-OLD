8ff62cf8db62cf8363319a6bafe64bfe6a7ea366
Godzilla0userbot <72538870+Godzilla0userbot@users.noreply.github.com>
2020-11-07 11:27:57 +0530

    Update tts.py

--- SaitamaRobot/modules/tts.py
+++ SaitamaRobot/modules/tts.py
@@ -52,6 +52,6 @@ __help__ = """
 TTS_HANDLER = DisableAbleCommandHandler("tts", tts, pass_args=True)
 dispatcher.add_handler(TTS_HANDLER)
 
-__mod_name__ = "tts"
+__mod_name__ = "Tts"
 __command_list__ = ["tts"]
 __handlers__ = [TTS_HANDLER]
