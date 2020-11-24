1753dc3d0e9b9467ca2c29720f87f4803c652e22
Sasuke Uchiha <73424435+FtSasaki@users.noreply.github.com>
2020-11-24 14:51:58 +0530

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -90,12 +90,12 @@ def ding(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    messageX = msg.reply_text("Dinging...")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
-    message.edit_text(
+    messageX.edit_text(
         "! !DONG! !\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
