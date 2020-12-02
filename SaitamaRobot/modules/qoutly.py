da01701eda4561e00d94433af80ad04761876962
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-02 09:25:55 +0530

    Update qoutly.py

--- SaitamaRobot/modules/qoutly.py
+++ SaitamaRobot/modules/qoutly.py
@@ -10,7 +10,7 @@ import random
 import json
 import os
 import re
-from SaitamaRobot.events import register
+from SaitamaRobot.event import register
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
