1fa9ac5ff8c40422306130ed00d5900d62991480
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-05 20:25:17 +0530

    Update animequotes.py

--- SaitamaRobot/modules/animequotes.py
+++ SaitamaRobot/modules/animequotes.py
@@ -13,14 +13,6 @@ def animequotes(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(animequotesstring.ANIMEQUOTES))
    
 
-#FtSasaki Fixed The Anime Quotes Module (ㄒoㄒ)
-
-__mod_name__ = "Anime Quotes"
-
-__help__ = """
- • `animequotes` *:* Random Anime Motivational Quotes
- 
-"""
 
 ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
 
