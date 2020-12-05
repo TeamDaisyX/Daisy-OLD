b323cc233493f6e2d984a024b6d120a8f6dba49f
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-05 20:28:03 +0530

    Create animehelpstring.py

---
+++ SaitamaRobot/modules/animehelpstring.py
@@ -0,0 +1,17 @@
+__help__ = """
+Get information about anime, manga or characters from [AniList](anilist.co).
+
+*Available commands:*
+
+ • `/anime <anime>`*:* returns information about the anime.
+ • `/character <character>`*:* returns information about the character.
+ • `/animequote : Get random Anime qoute
+ • `/manga <manga>`*:* returns information about the manga.
+ • `/user <user>`*:* returns information about a MyAnimeList user.
+ • `/upcoming`*:* returns a list of new anime in the upcoming seasons.
+ • `/kaizoku <anime>`*:* search an anime on animekaizoku.com
+ • `/kayo <anime>`*:* search an anime on animekayo.com
+ • `/airing <anime>`*:* returns anime airing info.
+
+ """
+__mod_name__ = "Anime"
