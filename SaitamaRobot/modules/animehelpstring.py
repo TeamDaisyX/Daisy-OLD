f5db0b92bd1742e04d02158cfac01093af8ed9a5
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-07 10:33:30 +0530

    Update animehelpstring.py

--- SaitamaRobot/modules/animehelpstring.py
+++ SaitamaRobot/modules/animehelpstring.py
@@ -3,15 +3,15 @@ Get information about anime, manga or characters from [AniList](anilist.co).
 
 *Available commands:*
 
- -/animequotes*:* get random anime qoute.
- -/anime <anime>`*:* returns information about the anime.
- -/character <character>`*:* returns information about the character.
- -/manga <manga>`*:* returns information about the manga.
- -/user <user>`*:* returns information about a MyAnimeList user.
- -/upcoming`*:* returns a list of new anime in the upcoming seasons.
- -/kaizoku <anime>`*:* search an anime on animekaizoku.com
- -/kayo <anime>`*:* search an anime on animekayo.com
- -/airing <anime>`*:* returns anime airing info.
+ -`/animequotes`*:* get random anime qoute.
+ -`/anime <anime>`*:* returns information about the anime.
+ -`/character <character>`*:* returns information about the character.
+ -`/manga <manga>`*:* returns information about the manga.
+ -`/user <user>`*:* returns information about a MyAnimeList user.
+ -`/upcoming`*:* returns a list of new anime in the upcoming seasons.
+ -`/kaizoku <anime>`*:* search an anime on animekaizoku.com
+ -`/kayo <anime>`*:* search an anime on animekayo.com
+ -`/airing <anime>`*:* returns anime airing info.
 
  """
 __mod_name__ = "Anime"
