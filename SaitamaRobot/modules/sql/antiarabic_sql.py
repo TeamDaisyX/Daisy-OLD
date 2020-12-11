c535cd61d695d2b192e2de9d48ac02c2a59d2d7e
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-11 18:02:23 +0530

    Update antiarabic_sql.py

--- SaitamaRobot/modules/sql/antiarabic_sql.py
+++ SaitamaRobot/modules/sql/antiarabic_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean
 
-from hitsuki.modules.sql import SESSION, BASE
+from SaitamaRobot.modules.sql import SESSION, BASE
 
 
 class AntiArabicChatSettings(BASE):
