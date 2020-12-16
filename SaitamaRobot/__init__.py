ff3c8301f14c4443443cfa9fb484a57dc4e349b0
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-16 20:06:06 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -87,6 +87,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
+    REPOSITORY = os.environ.get('REPOSITORY', "")
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
