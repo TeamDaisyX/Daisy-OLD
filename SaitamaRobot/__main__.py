0311ff8cea053bc89580f01da889ed184bd9e063
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-17 23:04:51 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -474,7 +474,7 @@ def get_help(update, context):
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
         module = args[1].lower()
         text = (
-            "Here is the available help for the *{}* module:\n".format(
+            "*╠┮┄┄ ｢ Help for {} module 」┄┄┭╣*\n".format(
                 HELPABLE[module].__mod_name__
             )
             + HELPABLE[module].__help__
