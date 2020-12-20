a22c02bfe4e0f3ff8a473e6910046a8557ee0e7f
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-20 22:56:50 +0530

    Update misc.py

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -68,7 +68,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
         
     else:
-        pairs += [[EqInlineKeyboardButton("Back",  callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("<b>Back to Info</b>",  callback_data="aboutmanu_")]]
 
     return pairs
 
