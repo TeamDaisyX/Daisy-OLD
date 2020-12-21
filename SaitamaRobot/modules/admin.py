5bf5dc63e0378e5d216e58cb30a9ea8059eddbaa
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-21 19:38:33 +0530

    Update admin.py

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -454,17 +454,17 @@ def adminlist(update, context):
 
 
 __help__ = """
- • `/admins`*:* list of admins in the chat
+ ✪ `/admins`*:* list of admins in the chat
 
 *Admins only:*
- • `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
- • `/unpin`*:* unpins the currently pinned message
- • `/invitelink`*:* gets invitelink
- • `/promote`*:* promotes the user replied to
- • `/demote`*:* demotes the user replied to
- • `/title <title here>`*:* sets a custom title for an admin that the bot promoted
- • `/admincache`*:* force refresh the admins list
- • `/zombies`*:* scan and clean zombies
+ ✪ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+ ✪ `/unpin`*:* unpins the currently pinned message
+ ✪ `/invitelink`*:* gets invitelink
+ ✪ `/promote`*:* promotes the user replied to
+ ✪ `/demote`*:* demotes the user replied to
+ ✪ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
+ ✪ `/admincache`*:* force refresh the admins list
+ ✪ `/zombies`*:* scan and clean zombies
 """
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
