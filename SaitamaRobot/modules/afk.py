ce810bf4ce79831b6267411997e3792e6bbab20c
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-21 19:39:36 +0530

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -141,8 +141,8 @@ __mod_name__ = "AFK"
 
 
 __help__ = """
-  - /afk <reason>: Mark yourself as AFK.
-  - brb <reason>: Same as the afk command, but not a command.\n
+  ✪ `/afk <reason>`*:* Mark yourself as AFK.
+  ✪ `brb <reason>`*:* Same as the afk command, but not a command.\n
   When marked as AFK, any mentions will be replied to with a message stating that you're not available!
 """
 
