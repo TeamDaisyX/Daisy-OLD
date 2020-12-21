5786c03f9bc9e4a9ae7949cda30f56491b95ce46
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-21 20:05:34 +0530

    Update songs.py

--- SaitamaRobot/modules/songs.py
+++ SaitamaRobot/modules/songs.py
@@ -281,9 +281,9 @@ __help__ = """
  *You can either enter just the song name or both the artist and song
   name. *
 
-  /song <songname artist(optional)>: uploads the song in it's best quality available
-  /video <songname artist(optional)>: uploads the video song in it's best quality available
-  /lyrics <song>: returns the lyrics of that song.
+ ✪ `/song` <songname artist(optional)>: uploads the song in it's best quality available
+ ✪ `/video` <songname artist(optional)>: uploads the video song in it's best quality available
+ ✪ `/lyrics` <song>: returns the lyrics of that song.
 
 """
 
