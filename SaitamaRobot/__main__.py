0e5c4282aea0e7f8b9cecca32753beb086875e33
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-21 21:19:06 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -265,7 +265,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*✪-----  ｢  Help  for  {}  module 」-----✪*\n".format(
+                "*⚊❮❮❮❮ ｢  Help  for  {}  module 」❯❯❯❯⚊*\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
