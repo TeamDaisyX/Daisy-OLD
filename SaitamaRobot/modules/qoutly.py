fc2ad2df392227443c1c041b467f3d9c492962f7
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-22 17:18:01 +0530

    Update qoutly.py

--- SaitamaRobot/modules/qoutly.py
+++ SaitamaRobot/modules/qoutly.py
@@ -11,7 +11,7 @@ import json
 import os
 import re
 
-from SaitamaRobot.event import register
+from SaitamaRobot.events import register
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
