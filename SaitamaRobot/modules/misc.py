88da723d993edcc02461de428ca073b04cad96b5
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-24 18:54:05 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -554,49 +554,52 @@ def fpaste(update, context):
 
 @run_async
 def stats(update, context):
-    stats = f"┎─⌈ <b>Current {dispatcher.bot.first_name} Stats</b> ⌋\n┇\n" + "\n┋\n".join([mod.__stats__() for mod in STATS])
+    stats = f"┎─⌈ <b>Current {dispatcher.bot.first_name} Stats</b> ⌋\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r'(\d+)', r'<code>\1</code>', stats)
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
 
 
 # /ip is for private use
 __help__ = """
-*Info:*
- ✪ `/whois` :- Get information about user using pyrogram  method.
 
-*Translator:*
- ✪ `/tr` or `/tl`: - To translate to your language, by default language is set to english, use `/tr <lang code>` for some other language!
- ✪ `/splcheck`: - As a reply to get grammar corrected text of gibberish message.
- ✪ `/tts`: - To some message to convert it into audio format!
- ✪ `/stt` :- convert audio to text ( only English).
-
-*Use in bot pm* (admin can use in group).
- ✪ `/google` <text> :- search google queries.
-
-*Github:*
- ✪ `/git`: Returns info about a GitHub user or organization.
- ✪ `/repo`: Return the GitHub user or organization repository list (Limited at 40).
-
-*Covid*
- ✪ `/covid` :To get Global data.
- ✪ `/covid <country>`:To get data of a country.
- 
-*More:*
- ✪ `/paste`: Create a paste or a shortened url using [dogbin](https://del.dog)
- ✪ `/getpaste`: Get the content of a paste or shortened url from [dogbin](https://del.dog)
- ✪ `/wiki`: Search wikipedia articles.
- ✪ `/ud <query>`: Search stuffs in urban dictionary.
- ✪ `/wall <query>`: Get random wallpapers directly from bot! 
- ✪ `/weather <city>`: Gets weather information of particular place!
- ✪ `/reverse`: Reverse searches image or stickers on google.
  ✪ `/gdpr`: Deletes your information from the bot's database. Private chats only.
  ✪ `/markdownhelp`: Quick summary of how markdown works in telegram - can only be called in private chats.
  ✪ `/removebotkeyboard`: Got a nasty bot keyboard stuck in your group?
- ✪ `/app <app name>`: Finds an app in playstore for you
- ✪ `/cash`: currency converter
- ✪ `/time <query>`: Gives information about a timezone.
- ✪ `/plet <text>`: make ur text sticker in different colours
- ✪ `/imdb <text>`: check imdb score of anime or movies.
+
+*➩Info:*
+  ✪ `/whois` :- Get information about user using pyrogram  method.
+
+*➩Translator:*
+  ✪ `/tr` or `/tl`: - To translate to your language, by default language is set to english, use /tr <lang code> for some other language!
+  ✪ `/splcheck`: - As a reply to get grammar corrected text of gibberish message.
+  ✪ `/tts`: - To some message to convert it into audio format!
+  ✪ `/stt`:- convert audio to text ( only English).
+
+*➩Search:*
+  ✪ `/google <text>`:- search google queries.Use in bot pm (admin can use in group).
+  ✪ `/wiki`: Search wikipedia articles.
+  ✪ `/ud <query>`: Search stuffs in urban dictionary.
+  ✪ `/reverse`: Reverse searches image or stickers on google.
+  ✪ `/app <app name>`: Finds an app in playstore for you
+  ✪ `/cash`: currency converter
+  ✪ `/wall <query>`: Get random wallpapers directly from bot!
+
+*➩Github:*
+  ✪ `/git`: Returns info about a GitHub user or organization.
+  ✪ `/repo`: Return the GitHub user or organization repository list (Limited at 40).
+
+*➩Covid:*
+  ✪ /covid :To get Global data.
+  ✪ /covid <country>:To get data of a country.
+ 
+*➩Paste:*
+  ✪ `/paste`: Create a paste or a shortened url using dogbin. *From letters to url.*
+  ✪ `/getpaste`: Get the content of a paste or shortened url from dogbin
+  ✪ `/fpaste`: Create a paste or a shortened url using dogbin and nekobin.*From files to url.*
+
+*➩Time and Weather:*
+  ✪ `/time <query>`: Gives information about a timezone.
+  ✪ `/weather <city>`: Gets weather information of particular place!
 \
 """
 
