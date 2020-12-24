32e8bbbe01d31ea9f74534b178b05fba9456d3ad
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-24 12:32:36 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -46,6 +46,29 @@ async def purge_messages(event):
     text = f"Purged Successfully in {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode='markdown')
 
+async def delete_messages(event):
+    if event.from_id is None:
+        return
+
+    if not await user_is_admin(
+            user_id=event.sender_id, message=event) and event.from_id not in [
+                1087968824
+            ]:
+        await event.reply("Only Admins are allowed to use this command")
+        return
+
+    if not await can_delete_messages(message=event):
+        await event.reply("Can't seem to delete this?")
+        return
+
+    message = await event.get_reply_message()
+    if not message:
+        await event.reply("Whadya want to delete?")
+        return
+    chat = await event.get_input_chat()
+    del_message = [message, event.message]
+    await event.client.delete_messages(chat, del_message)
+    
 async def spurge_messages(event):
     start = time.perf_counter()
     if event.from_id is None:
@@ -77,32 +100,6 @@ async def spurge_messages(event):
         if len(messages) == 100:
             await event.client.delete_messages(event.chat_id, messages)
             messages = []
-            
-    try:
-        await event.client.delete_messages(event.chat_id, messages)        
-            
-async def delete_messages(event):
-    if event.from_id is None:
-        return
-
-    if not await user_is_admin(
-            user_id=event.sender_id, message=event) and event.from_id not in [
-                1087968824
-            ]:
-        await event.reply("Only Admins are allowed to use this command")
-        return
-
-    if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
-        return
-
-    message = await event.get_reply_message()
-    if not message:
-        await event.reply("Whadya want to delete?")
-        return
-    chat = await event.get_input_chat()
-    del_message = [message, event.message]
-    await event.client.delete_messages(chat, del_message)
 
 
 __help__ = """
