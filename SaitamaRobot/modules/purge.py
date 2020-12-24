8941eee3a9ec734a052b05ed4ecd7aafcad9bb53
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-24 12:23:51 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -43,10 +43,44 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f""
+    text = f"Purged Successfully in {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode='markdown')
 
+async def spurge_messages(event):
+    start = time.perf_counter()
+    if event.from_id is None:
+        return
 
+    if not await user_is_admin(
+            user_id=event.sender_id, message=event) and event.from_id not in [
+                1087968824
+            ]:
+        await event.reply("Only Admins are allowed to use this command")
+        return
+
+    if not await can_delete_messages(message=event):
+        await event.reply("Can't seem to purge the message")
+        return
+
+    reply_msg = await event.get_reply_message()
+    if not reply_msg:
+        await event.reply(
+            "Reply to a message to select where to start purging from.")
+        return
+    messages = []
+    message_id = reply_msg.id
+    delete_to = event.message.id
+
+    messages.append(event.reply_to_msg_id)
+    for msg_id in range(message_id, delete_to + 1):
+        messages.append(msg_id)
+        if len(messages) == 100:
+            await event.client.delete_messages(event.chat_id, messages)
+            messages = []
+            
+    try:
+        await event.client.delete_messages(event.chat_id, messages)        
+            
 async def delete_messages(event):
     if event.from_id is None:
         return
@@ -80,10 +114,13 @@ __help__ = """
 
 PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
 DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
+SPURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]spurge$")
+
 
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
+telethn.add_event_handler(*SPURGE_HANDLER)
 
 __mod_name__ = "Purges"
-__command_list__ = ["del", "purge"]
-__handlers__ = [PURGE_HANDLER, DEL_HANDLER]
+__command_list__ = ["del", "purge", "spurge"]
+__handlers__ = [PURGE_HANDLER, DEL_HANDLER, SPURGE_HANDLER]
