bd0102fb02b4cb6b5604ce47d755f8e558e744ee
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-02 10:43:36 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -615,7 +615,7 @@ __help__ = """
 \
 """
 
-__mod_name__ = "Extra"
+__mod_name__ = "Miscs"
 
 APP_HANDLER = DisableAbleCommandHandler("app", app)
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
