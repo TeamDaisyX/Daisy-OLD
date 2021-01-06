9e234473d1e999964cbe02a1a5956d7b33768fa2
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-06 17:38:11 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -298,7 +298,7 @@ def markdown_help(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton(
                     "Markdown help",
-                    url=f"t.me/{context.bot.username}?start=markdownhelp")
+                    url=f"t.me/{}?start=markdownhelp")
             ]]))
         return
     markdown_help_sender(update)
