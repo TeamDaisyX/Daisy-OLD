855b9f95969578a4ff16f507b7112a01d4b7decd
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-06 23:16:22 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -531,6 +531,7 @@ def send_settings(chat_id, user_id, user=False):
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     paginate_modules(0, CHAT_SETTINGS, "stngs", chat=chat_id)
+                ),
                 )
             )
         else:
