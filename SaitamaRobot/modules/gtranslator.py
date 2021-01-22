887b1638f14312ea7ddcbac85a1e531fe98c5e16
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-22 23:11:24 +0530

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -1,5 +1,5 @@
 from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator
+from googletrans import LANGUAGES, Translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
