4d069d030d99b828a4d28f58c2f8756271911cdd
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:13:46 +0530

    Update approval.py

--- SaitamaRobot/modules/approval.py
+++ SaitamaRobot/modules/approval.py
@@ -208,12 +208,12 @@ Sometimes, you might trust a user not to send unwanted content.
 Maybe not enough to make them admin, but you might be ok with auto warns, blacklists, and antiflood not applying to them.
 That's what approvals are for - approve of trustworthy users to allow them to send 
 Admin commands:
-✪ `/approval`: Check a user's approval status in this chat.
+✪ /approval*:* Check a user's approval status in this chat.
 Admin commands:
-✪ `/approve`: Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-✪ `/unapprove`: Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-✪ `/approved`: List all approved users.
-✪ `/unapproveall`: Unapprove ALL users in a chat. This cannot be undone.
+✪ /approve*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+✪ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+✪ /approved*:* List all approved users.
+✪ /unapproveall*:* Unapprove ALL users in a chat. This cannot be undone.
 \
 """    
 
