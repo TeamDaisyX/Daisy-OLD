4c593f0626bc816e3406c5d152de74e656d941f5
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:24:45 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -498,25 +498,24 @@ def __user_info__(user_id):
 
 __help__ = """
 *ID:*
- ✪ `/id`*:* get the current group id. If used by replying to a message, gets that user's id.
- ✪ `/gifid`*:* reply to a gif to me to tell you its file ID.
+ ✪ /id*:* get the current group id. If used by replying to a message, gets that user's id.
 
 *Self addded information:* 
- ✪ `/setme <text>`*:* will set your info
- ✪ `/me`*:* will get your or another user's info.
+ ✪ /setme <text>*:* will set your info
+ ✪ /me*:* will get your or another user's info.
 _Examples:_
  `/setme I am a wolf.`
  `/me @username(defaults to yours if no user specified)`
 
 *Information others add on you:* 
- ✪ `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
- ✪ `/setbio <text>`*:* while replying, will save another user's bio 
+ ✪ /bio*:* will get your or another user's bio. This cannot be set by yourself.
+ ✪ /setbio <text>*:* while replying, will save another user's bio 
 _Examples:_
  `/bio @username(defaults to yours if not specified).`
  `/setbio This user is a wolf` (reply to the user)
 
 *Overall Information about you:*
- ✪ `/info`*:* get information about a user. 
+ ✪ /info*:* get information about a user. 
  
 *What is that health thingy?*
  Come and see [HP System explained](https://t.me/SuzuyaUpdates/33)
