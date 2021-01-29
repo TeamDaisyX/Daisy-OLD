6f7ffcb89241bf35367929fb90be4fadc8d24b65
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:50:17 +0530

    Update muting.py

--- SaitamaRobot/modules/muting.py
+++ SaitamaRobot/modules/muting.py
@@ -351,11 +351,11 @@ def stemp_mute(update: Update, context: CallbackContext) -> str:
 
 __help__ = """
 *Admins only:*
- ✪ `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- ✪ `/smute <userhandle>`*:* silences a user without notifying. Can also be used as a reply, muting the replied to user.
- ✪ `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪ `/stmute <userhandle> x(m/h/d)`*:* mutes a user for x time without notifying. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪ `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+ ✪ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
+ ✪ /smute <userhandle>*:* silences a user without notifying. Can also be used as a reply, muting the replied to user.
+ ✪ /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ ✪ /stmute <userhandle> x(m/h/d)*:* mutes a user for x time without notifying. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ ✪ /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
  
  _NOTE:_
  If you set Log Channels, you will get logs of Silent mutes. Check *Logger* module to know more about Log Channel.
