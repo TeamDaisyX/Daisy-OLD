bd4798cc74a4f2dcc3c6b10e3ea79cac829c90b4
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:55:34 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -665,16 +665,16 @@ def remove_fvrtsticker(update, context):
 __help__ = """
 Stickers made easy with stickers module!
 
-✪ `/stickers`: Find stickers for given term on combot sticker catalogue 
+✪ /stickers*:* Find stickers for given term on combot sticker catalogue 
 
-✪ `/addsticker`: Reply to a sticker to add it to your pack.
-✪ `/delsticker`: Reply to your anime exist sticker to your pack to delete it.
-✪ `/stickerid`: Reply to a sticker to me to tell you its file ID.
-✪ `/gifid`: Reply to a gif to me to tell you its file ID.
-✪ `/getsticker`: Reply to a sticker to me to upload its raw PNG file.
-✪ `/addfsticker` or `/afs` <custom name>: Reply to a sticker to add it into your favorite pack list.
-✪ `/myfsticker` or `/mfs`: Get list of your favorite packs.
-✪ `/removefsticker` or `/rfs` <custom name>: Reply to a sticker to remove it into your favorite pack list.
+✪ /addsticker*:* Reply to a sticker to add it to your pack.
+✪ /delsticker*:* Reply to your anime exist sticker to your pack to delete it.
+✪ /stickerid*:* Reply to a sticker to me to tell you its file ID.
+✪ /gifid*:* Reply to a gif to me to tell you its file ID.
+✪ /getsticker*:* Reply to a sticker to me to upload its raw PNG file.
+✪ /addfsticker or /afs <custom name>*:* Reply to a sticker to add it into your favorite pack list.
+✪ /myfsticker or /mfs*:* Get list of your favorite packs.
+✪ /removefsticker or /rfs <custom name>*:* Reply to a sticker to remove it into your favorite pack list.
 
 *Example:* `/addfsticker my cool pack`
 """
