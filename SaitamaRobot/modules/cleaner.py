c271ba6d6b4df71cd7f5bf2d3a641e1bec9cbbb0
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:17:13 +0530

    Update cleaner.py

--- SaitamaRobot/modules/cleaner.py
+++ SaitamaRobot/modules/cleaner.py
@@ -206,14 +206,14 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
 
 __help__ = """
 *Blue text cleaner removed any made up commands that people send in your chat.*
- ✪ `/cleanblue <on/off/yes/no>`*:* clean commands after sending
- ✪ `/ignoreblue <word>`*:* prevent auto cleaning of the command
- ✪ `/unignoreblue <word>`*:* remove prevent auto cleaning of the command
- ✪ `/listblue`*:* list currently whitelisted commands
+ ✪ /cleanblue <on/off/yes/no>*:* clean commands after sending
+ ✪ /ignoreblue <word>*:* prevent auto cleaning of the command
+ ✪ /unignoreblue <word>*:* remove prevent auto cleaning of the command
+ ✪ /listblue*:* list currently whitelisted commands
  
  *Following are Disasters only commands, admins cannot use these:*
- ✪ `/gignoreblue <word>`*:* globally ignores bluetext cleaning of saved word across Suzuya.
- ✪ `/ungignoreblue <word>`*:* remove said command from global cleaning list
+ ✪ /gignoreblue <word>*:* globally ignores bluetext cleaning of saved word across Suzuya.
+ ✪ /ungignoreblue <word>*:* remove said command from global cleaning list
 """
 
 SET_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("cleanblue",
