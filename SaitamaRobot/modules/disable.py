058cacd03f61e0158b0c96c06e7d5b81fa3043a7
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:26:06 +0530

    Update disable.py

--- SaitamaRobot/modules/disable.py
+++ SaitamaRobot/modules/disable.py
@@ -334,14 +334,14 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(TOGGLE_HANDLER)
 
     __help__ = """
-    ✪ `/cmds`*:* check the current status of disabled commands
+    ✪ /cmds*:* check the current status of disabled commands
 
     *Admins only:*
-    ✪ `/enable <cmd name>`*:* enable that command
-    ✪ `/disable <cmd name>`*:* disable that command
-    ✪ `/enablemodule <module name>`*:* enable all commands in that module
-    ✪ `/disablemodule <module name>`*:* disable all commands in that module
-    ✪ `/listcmds`*:* list all possible toggleable commands
+    ✪ /enable <cmd name>*:* enable that command
+    ✪ /disable <cmd name>*:* disable that command
+    ✪ /enablemodule <module name>*:* enable all commands in that module
+    ✪ /disablemodule <module name>*:* disable all commands in that module
+    ✪ /listcmds*:* list all possible toggleable commands
     """
 
     __mod_name__ = "Disabling"
