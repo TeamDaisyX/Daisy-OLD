897b3d9a0db01eac7d90500e975c2ebe326f77cb
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:39:01 +0530

    Update locks.py

--- SaitamaRobot/modules/locks.py
+++ SaitamaRobot/modules/locks.py
@@ -633,12 +633,12 @@ Do stickers annoy you? or want to avoid people sharing links? or pictures? \
 You're in the right place!
 The locks module allows you to lock away some common items in the \
 telegram world; the bot will automatically delete them!
- ✪ `/locktypes`*:* Lists all possible locktypes
+ ✪ /locktypes*:* Lists all possible locktypes
  
 *Admins only:*
- ✪ `/lock <type>`*:* Lock items of a certain type (not available in private)
- ✪ `/unlock <type>`*:* Unlock items of a certain type (not available in private)
- ✪ `/locks`*:* The current list of locks in this chat.
+ ✪ /lock <type>*:* Lock items of a certain type (not available in private)
+ ✪ /unlock <type>*:* Unlock items of a certain type (not available in private)
+ ✪ /locks*:* The current list of locks in this chat.
  
 Locks can be used to restrict a group's users.
 eg:
