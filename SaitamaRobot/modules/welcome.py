7c1c89e6fca2d1fe2333f1532783ec20463f027b
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-01-29 23:38:02 +0530

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -1011,17 +1011,17 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
 *Admins only:*
- ✪ `/welcome <on/off>`*:* enable/disable welcome messages.
- ✪ `/welcome`*:* shows current welcome settings.
- ✪ `/welcome noformat`*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
- ✪ `/goodbye`*:* same usage and args as `/welcome`.
- ✪ `/setwelcome <sometext>`*:* set a custom welcome message. If used replying to media, uses that media.
- ✪ `/setgoodbye <sometext>`*:* set a custom goodbye message. If used replying to media, uses that media.
- ✪ `/resetwelcome`*:* reset to the default welcome message.
- ✪ `/resetgoodbye`*:* reset to the default goodbye message.
- ✪ `/cleanwelcome <on/off>`*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
- ✪ `/welcomemutehelp`*:* gives information about welcome mutes.
- ✪ `/cleanservice <on/off`*:* deletes telegrams welcome/left service messages. 
+ ✪ /welcome <on/off>*:* enable/disable welcome messages.
+ ✪ /welcome*:* shows current welcome settings.
+ ✪ /welcome noformat*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
+ ✪ /goodbye*:* same usage and args as `/welcome`.
+ ✪ /setwelcome <sometext>*:* set a custom welcome message. If used replying to media, uses that media.
+ ✪ /setgoodbye <sometext>*:* set a custom goodbye message. If used replying to media, uses that media.
+ ✪ /resetwelcome*:* reset to the default welcome message.
+ ✪ /resetgoodbye*:* reset to the default goodbye message.
+ ✪ /cleanwelcome <on/off>*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
+ ✪ /welcomemutehelp*:* gives information about welcome mutes.
+ ✪ /cleanservice <on/off*:* deletes telegrams welcome/left service messages. 
  *Example:*
 user joined chat, user left chat.
 
