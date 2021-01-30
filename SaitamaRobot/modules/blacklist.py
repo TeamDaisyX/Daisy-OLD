7cd9f6eaa6b6901d61855ca8ba211d88d222a173
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-30 19:31:47 +0530

    Update blacklist.py

--- SaitamaRobot/modules/blacklist.py
+++ SaitamaRobot/modules/blacklist.py
@@ -342,17 +342,8 @@ def del_blacklist(update, context):
     if not to_match:
         return
 
-
-    chat_id = str(chat.id)[1:] 
-    approve_list = list(REDIS.sunion(f'approve_list_{chat_id}'))
-    target_user = mention_html(user.id, user.first_name)
-    if target_user in approve_list:
-        return
-
-
     getmode, value = sql.get_blacklist_setting(chat.id)
 
-   	
     chat_filters = sql.get_chat_blacklist(chat.id)
     for trigger in chat_filters:
         pattern = r"( |^|[^\w])" + re.escape(trigger) + r"( |$|[^\w])"
@@ -361,15 +352,9 @@ def del_blacklist(update, context):
                 if getmode == 0:
                     return
                 elif getmode == 1:
-                    try:
-                        message.delete()
-                    except BadRequest:
-                        pass
+                    message.delete()
                 elif getmode == 2:
-                    try:
-                        message.delete()
-                    except BadRequest:
-                        pass
+                    message.delete()
                     warn(
                         update.effective_user,
                         chat,
@@ -462,26 +447,13 @@ def __stats__():
 __mod_name__ = "Blacklists"
 
 __help__ = """
-➩*Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!*
-
-*NOTE*: *Blacklists do not affect group admins.*
- ✪ /blacklist*:* View the current blacklisted words.
-☆*Admin only:*
- ✪ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- ✪ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- ✪ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
- 
-➩*Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.*
-
-*NOTE:* *Blacklist stickers do not affect the group admin.*
- ✪ /blsticker*:* See current blacklisted sticker
-☆*Admins Only:*
- ✪ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
- ✪ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
- ✪ /rmblsticker <sticker link>*:* Same as above
- ✪ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
-*Note:*
- ✪ `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+*NOTE*: Blacklists do not affect group admins.
+ • `/blacklist`*:* View the current blacklisted words.
+Admin only:
+ • `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ • `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+ • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
     "blacklist", blacklist, pass_args=True, admin_ok=True)
