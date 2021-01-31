cdf3e1537e910abc69abeaa6c57a1de2490581c7
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-01-31 11:16:11 +0530

    Update zipper.py

--- SaitamaRobot/modules/zipper.py
+++ SaitamaRobot/modules/zipper.py
@@ -194,8 +194,8 @@ def get_lst_of_files(input_directory, output_lst):
 
 
 __help__ = """
- - /zip: reply to a telegram file to compress it in .zip format
- - /unzip: reply to a telegram file to decompress it from the .zip format
+ ✪ /zip: reply to a telegram file to compress it in .zip format
+ ✪ /unzip: reply to a telegram file to decompress it from the .zip format
 """
 
 __mod_name__ = "Zipper"
