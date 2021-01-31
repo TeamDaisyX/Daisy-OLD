29e2542db71e921d4f3c50d9e464c167de350043
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-31 15:59:46 +0530

    Create MoreTools.py

---
+++ SaitamaRobot/modules/MoreTools.py
@@ -0,0 +1,20 @@
+__help__ = """
+     ** Locate **
+ - /locate <Place> : Show Location on a map
+ 
+     ** Image Tools**
+ - /img <text>: perform a image search
+ - /getqr <reply to image>: Read QR code
+ - /makeqr <reply to text>: Make QR code
+ 
+     ** Style Text **
+ - /weebify : Weebify Text
+ - /square : square Text
+ - /blue : Blues text
+ 
+    ** More **
+ - /phone <tp no> : Track Phone no
+ 
+ 
+"""
+__mod_name__ = "More Tools"
