02c1e731bcfa321bff8942b647cf59c0c26b291a
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-03 18:22:54 +0530

    Update locks.py

--- SnowGirl/modules/locks.py
+++ SnowGirl/modules/locks.py
@@ -576,7 +576,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "Locks 🔐"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
