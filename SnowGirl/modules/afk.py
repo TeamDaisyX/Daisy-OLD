d88910f50d1c5bc4238efdd64c0b076d7091afb1
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-03 17:22:17 +0530

    Update afk.py

--- SnowGirl/modules/afk.py
+++ SnowGirl/modules/afk.py
@@ -167,7 +167,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+__mod_name__ = "AFK 🏃‍♀️"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
