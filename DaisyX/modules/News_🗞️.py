b7bdc138fd82c6c2f26ac836961484499bb6e978
InukaAsith <inukaasith7@gmail.com>
2021-02-04 14:08:27 +0000

    pylint: auto fixes

--- DaisyX/modules/News_🗞️.py
+++ DaisyX/modules/News_🗞️.py
@@ -1,20 +1,16 @@
-from julia import telethn as tbot
-from DaisyX import CMD_HELP
 import os
-import urllib.request
-from typing import List
-from typing import Optional
 from urllib.request import urlopen
+
 import bs4
+from julia import *
+from julia import telethn as tbot
 from pymongo import MongoClient
 from telethon import *
 from telethon import events
-from telethon.tl import functions
-from telethon.tl import types
+from telethon.tl import functions, types
 from telethon.tl.types import *
 
-from julia import *
-from julia.events import register
+from DaisyX import CMD_HELP
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
@@ -46,10 +42,10 @@ async def is_register_admin(chat, user):
 async def _(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -62,16 +58,30 @@ async def _(event):
     chatid = event.chat_id
     msg = await tbot.send_message(chatid, "Loading ...")
     msgid = msg.id
-    await tbot.edit_message(chatid, msgid, "Click on the below button to read the latest news headlines 👇", buttons=[[Button.inline('▶️', data=f'news-{sender}|{country}|{lang}|{index}|{chatid}|{msgid}')], [Button.inline('❌', data=f'newsstop-{sender}|{chatid}|{msgid}')]])
+    await tbot.edit_message(
+        chatid,
+        msgid,
+        "Click on the below button to read the latest news headlines 👇",
+        buttons=[
+            [
+                Button.inline(
+                    "▶️",
+                    data=f"news-{sender}|{country}|{lang}|{index}|{chatid}|{msgid}",
+                )
+            ],
+            [Button.inline("❌", data=f"newsstop-{sender}|{chatid}|{msgid}")],
+        ],
+    )
+
 
 @tbot.on(events.CallbackQuery(pattern=r"news(\-(.*))"))
 async def paginate_news(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -79,14 +89,14 @@ async def paginate_news(event):
             return
     tata = event.pattern_match.group(1)
     data = tata.decode()
-    meta = data.split('-', 1)[1]
-    #print(meta)
+    meta = data.split("-", 1)[1]
+    # print(meta)
     if "|" in meta:
         sender, country, lang, index, chatid, msgid = meta.split("|")
     sender = int(sender.strip())
     if not event.sender_id == sender:
-       await event.answer("You haven't send that command !")
-       return
+        await event.answer("You haven't send that command !")
+        return
     country = country.strip()
     lang = lang.strip()
     index = int(index.strip())
@@ -95,29 +105,54 @@ async def paginate_news(event):
     msgid = int(msgid.strip())
     news_url = f"https://news.google.com/rss?hl={lang}-{country}&gl={country}&ceid={country}:{lang}"
     try:
-      Client = urlopen(news_url)
+        Client = urlopen(news_url)
     except Exception:
-      await event.reply("Invalid country or language code provided.")
-      return
+        await event.reply("Invalid country or language code provided.")
+        return
     xml_page = Client.read()
     Client.close()
-    soup_page = bs4.BeautifulSoup(xml_page, 'xml')
+    soup_page = bs4.BeautifulSoup(xml_page, "xml")
     news_list = soup_page.find_all("item")
     header = f"**#{num} **"
     title = news_list[int(num)].title.text
     text = news_list[int(num)].link.text
     date = news_list[int(num)].pubDate.text
-    lastisthis = f"{header}[{title}]({text})"+"\n\n"+ f"`{date}`"
-    await tbot.edit_message(chatid, msgid, lastisthis, link_preview=False, buttons=[[Button.inline('◀️', data=f'prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}'), Button.inline('❌', data=f'newsstop-{sender}|{chatid}|{msgid}'), Button.inline('▶️', data=f'nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}')], [Button.inline('Refresh 🔁', data=f'newnews-{sender}|{country}|{lang}|{chatid}|{msgid}')]])
+    lastisthis = f"{header}[{title}]({text})" + "\n\n" + f"`{date}`"
+    await tbot.edit_message(
+        chatid,
+        msgid,
+        lastisthis,
+        link_preview=False,
+        buttons=[
+            [
+                Button.inline(
+                    "◀️",
+                    data=f"prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+                Button.inline("❌", data=f"newsstop-{sender}|{chatid}|{msgid}"),
+                Button.inline(
+                    "▶️",
+                    data=f"nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+            ],
+            [
+                Button.inline(
+                    "Refresh 🔁",
+                    data=f"newnews-{sender}|{country}|{lang}|{chatid}|{msgid}",
+                )
+            ],
+        ],
+    )
+
 
 @tbot.on(events.CallbackQuery(pattern=r"prevnews(\-(.*))"))
 async def paginate_prevnews(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -125,14 +160,14 @@ async def paginate_prevnews(event):
             return
     tata = event.pattern_match.group(1)
     data = tata.decode()
-    meta = data.split('-', 1)[1]
-    #print(meta)
+    meta = data.split("-", 1)[1]
+    # print(meta)
     if "|" in meta:
         sender, country, lang, index, chatid, msgid = meta.split("|")
     sender = int(sender.strip())
     if not event.sender_id == sender:
-       await event.answer("You haven't send that command !")
-       return
+        await event.answer("You haven't send that command !")
+        return
     country = country.strip()
     lang = lang.strip()
     index = int(index.strip())
@@ -141,34 +176,59 @@ async def paginate_prevnews(event):
     msgid = int(msgid.strip())
     news_url = f"https://news.google.com/rss?hl={lang}-{country}&gl={country}&ceid={country}:{lang}"
     try:
-      Client = urlopen(news_url)
+        Client = urlopen(news_url)
     except Exception:
-      await event.reply("Invalid country or language code provided.")
-      return
+        await event.reply("Invalid country or language code provided.")
+        return
     xml_page = Client.read()
     Client.close()
-    soup_page = bs4.BeautifulSoup(xml_page, 'xml')
+    soup_page = bs4.BeautifulSoup(xml_page, "xml")
     news_list = soup_page.find_all("item")
     vector = len(news_list)
     if num < 0:
-       num = vector - 1
-    #print(vector)
-    #print(num)
+        num = vector - 1
+    # print(vector)
+    # print(num)
     header = f"**#{num} **"
     title = news_list[int(num)].title.text
     text = news_list[int(num)].link.text
     date = news_list[int(num)].pubDate.text
-    lastisthis = f"{header}[{title}]({text})"+"\n\n"+ f"`{date}`"
-    await tbot.edit_message(chatid, msgid, lastisthis, link_preview=False, buttons=[[Button.inline('◀️', data=f'prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}'), Button.inline('❌', data=f'newsstop-{sender}|{chatid}|{msgid}'), Button.inline('▶️', data=f'nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}')], [Button.inline('Refresh 🔁', data=f'newnews-{sender}|{country}|{lang}|{chatid}|{msgid}')]])
+    lastisthis = f"{header}[{title}]({text})" + "\n\n" + f"`{date}`"
+    await tbot.edit_message(
+        chatid,
+        msgid,
+        lastisthis,
+        link_preview=False,
+        buttons=[
+            [
+                Button.inline(
+                    "◀️",
+                    data=f"prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+                Button.inline("❌", data=f"newsstop-{sender}|{chatid}|{msgid}"),
+                Button.inline(
+                    "▶️",
+                    data=f"nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+            ],
+            [
+                Button.inline(
+                    "Refresh 🔁",
+                    data=f"newnews-{sender}|{country}|{lang}|{chatid}|{msgid}",
+                )
+            ],
+        ],
+    )
+
 
 @tbot.on(events.CallbackQuery(pattern=r"nextnews(\-(.*))"))
 async def paginate_nextnews(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -176,14 +236,14 @@ async def paginate_nextnews(event):
             return
     tata = event.pattern_match.group(1)
     data = tata.decode()
-    meta = data.split('-', 1)[1]
-    #print(meta)
+    meta = data.split("-", 1)[1]
+    # print(meta)
     if "|" in meta:
         sender, country, lang, index, chatid, msgid = meta.split("|")
     sender = int(sender.strip())
     if not event.sender_id == sender:
-       await event.answer("You haven't send that command !")
-       return
+        await event.answer("You haven't send that command !")
+        return
     country = country.strip()
     lang = lang.strip()
     index = int(index.strip())
@@ -192,32 +252,57 @@ async def paginate_nextnews(event):
     msgid = int(msgid.strip())
     news_url = f"https://news.google.com/rss?hl={lang}-{country}&gl={country}&ceid={country}:{lang}"
     try:
-      Client = urlopen(news_url)
+        Client = urlopen(news_url)
     except Exception:
-      await event.reply("Invalid country or language code provided.")
-      return
+        await event.reply("Invalid country or language code provided.")
+        return
     xml_page = Client.read()
     Client.close()
-    soup_page = bs4.BeautifulSoup(xml_page, 'xml')
+    soup_page = bs4.BeautifulSoup(xml_page, "xml")
     news_list = soup_page.find_all("item")
     vector = len(news_list)
     if num > vector - 1:
-       num = 0
+        num = 0
     header = f"**#{num} **"
     title = news_list[int(num)].title.text
     text = news_list[int(num)].link.text
     date = news_list[int(num)].pubDate.text
-    lastisthis = f"{header}[{title}]({text})"+"\n\n"+ f"`{date}`"
-    await tbot.edit_message(chatid, msgid, lastisthis, link_preview=False, buttons=[[Button.inline('◀️', data=f'prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}'), Button.inline('❌', data=f'newsstop-{sender}|{chatid}|{msgid}'), Button.inline('▶️', data=f'nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}')], [Button.inline('Refresh 🔁', data=f'newnews-{sender}|{country}|{lang}|{chatid}|{msgid}')]])
+    lastisthis = f"{header}[{title}]({text})" + "\n\n" + f"`{date}`"
+    await tbot.edit_message(
+        chatid,
+        msgid,
+        lastisthis,
+        link_preview=False,
+        buttons=[
+            [
+                Button.inline(
+                    "◀️",
+                    data=f"prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+                Button.inline("❌", data=f"newsstop-{sender}|{chatid}|{msgid}"),
+                Button.inline(
+                    "▶️",
+                    data=f"nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+            ],
+            [
+                Button.inline(
+                    "Refresh 🔁",
+                    data=f"newnews-{sender}|{country}|{lang}|{chatid}|{msgid}",
+                )
+            ],
+        ],
+    )
+
 
 @tbot.on(events.CallbackQuery(pattern=r"newsstop(\-(.*))"))
 async def newsstop(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -225,26 +310,27 @@ async def newsstop(event):
             return
     tata = event.pattern_match.group(1)
     data = tata.decode()
-    meta = data.split('-', 1)[1]
-    #print(meta)
+    meta = data.split("-", 1)[1]
+    # print(meta)
     if "|" in meta:
         sender, chatid, msgid = meta.split("|")
     sender = int(sender.strip())
     chatid = int(chatid.strip())
     msgid = int(msgid.strip())
     if not event.sender_id == sender:
-       await event.answer("You haven't send that command !")
-       return
+        await event.answer("You haven't send that command !")
+        return
     await tbot.edit_message(chatid, msgid, "Thanks for reading.\n❤️ from Google News !")
+
 
 @tbot.on(events.CallbackQuery(pattern=r"newnews(\-(.*))"))
 async def paginate_nextnews(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
-        iid = ch['id']
-        userss = ch['user']
+        iid = ch["id"]
+        userss = ch["user"]
     if event.is_group:
-        if (await is_register_admin(event.input_chat, event.sender_id)):
+        if await is_register_admin(event.input_chat, event.sender_id):
             pass
         elif event.chat_id == iid and event.sender_id == userss:
             pass
@@ -252,14 +338,14 @@ async def paginate_nextnews(event):
             return
     tata = event.pattern_match.group(1)
     data = tata.decode()
-    meta = data.split('-', 1)[1]
-    #print(meta)
+    meta = data.split("-", 1)[1]
+    # print(meta)
     if "|" in meta:
         sender, country, lang, chatid, msgid = meta.split("|")
     sender = int(sender.strip())
     if not event.sender_id == sender:
-       await event.answer("You haven't send that command !")
-       return
+        await event.answer("You haven't send that command !")
+        return
     country = country.strip()
     lang = lang.strip()
     num = 0
@@ -267,23 +353,47 @@ async def paginate_nextnews(event):
     msgid = int(msgid.strip())
     news_url = f"https://news.google.com/rss?hl={lang}-{country}&gl={country}&ceid={country}:{lang}"
     try:
-      Client = urlopen(news_url)
+        Client = urlopen(news_url)
     except Exception:
-      await event.reply("Invalid country or language code provided.")
-      return
+        await event.reply("Invalid country or language code provided.")
+        return
     xml_page = Client.read()
     Client.close()
-    soup_page = bs4.BeautifulSoup(xml_page, 'xml')
+    soup_page = bs4.BeautifulSoup(xml_page, "xml")
     news_list = soup_page.find_all("item")
     vector = len(news_list)
     if num > vector - 1:
-       num = 0
+        num = 0
     header = f"**#{num} **"
     title = news_list[int(num)].title.text
     text = news_list[int(num)].link.text
     date = news_list[int(num)].pubDate.text
-    lastisthis = f"{header}[{title}]({text})"+"\n\n"+ f"`{date}`"
-    await tbot.edit_message(chatid, msgid, lastisthis, link_preview=False, buttons=[[Button.inline('◀️', data=f'prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}'), Button.inline('❌', data=f'newsstop-{sender}|{chatid}|{msgid}'), Button.inline('▶️', data=f'nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}')], [Button.inline('Refresh 🔁', data=f'newnews-{sender}|{country}|{lang}|{chatid}|{msgid}')]])
+    lastisthis = f"{header}[{title}]({text})" + "\n\n" + f"`{date}`"
+    await tbot.edit_message(
+        chatid,
+        msgid,
+        lastisthis,
+        link_preview=False,
+        buttons=[
+            [
+                Button.inline(
+                    "◀️",
+                    data=f"prevnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+                Button.inline("❌", data=f"newsstop-{sender}|{chatid}|{msgid}"),
+                Button.inline(
+                    "▶️",
+                    data=f"nextnews-{sender}|{country}|{lang}|{num}|{chatid}|{msgid}",
+                ),
+            ],
+            [
+                Button.inline(
+                    "Refresh 🔁",
+                    data=f"newnews-{sender}|{country}|{lang}|{chatid}|{msgid}",
+                )
+            ],
+        ],
+    )
 
 
 file_help = os.path.basename(__file__)
@@ -296,9 +406,4 @@ __help__ = """
  - /news US en: This will return news for US in english language.
 """
 
-CMD_HELP.update({
-    file_helpo: [
-        file_helpo,
-        __help__
-    ]
-})
+CMD_HELP.update({file_helpo: [file_helpo, __help__]})
