25ee66264eddda00dab3e31d0b77e318e874c33b
InukaAsith <inukaasith7@gmail.com>
2021-02-04 14:23:24 +0000

    pylint: auto fixes

--- DaisyX/modules/Anti-Virus_🐞.py
+++ DaisyX/modules/Anti-Virus_🐞.py
@@ -1,19 +1,19 @@
 import os
-from DaisyX import telethn as tbot
-from DaisyX import VIRUS_API_KEY
-from telethon import events
-from telethon.tl import functions
-from telethon.tl import types
-from telethon.tl.types import MessageMediaDocument, DocumentAttributeFilename
-from pymongo import MongoClient
-from DaisyX import MONGO_DB_URI
-from DaisyX.events import register
+
 import cloudmersive_virus_api_client
+from pymongo import MongoClient
+from telethon.tl import functions, types
+from telethon.tl.types import DocumentAttributeFilename, MessageMediaDocument
+
+from DaisyX import MONGO_DB_URI, VIRUS_API_KEY
+from DaisyX import telethn as tbot
+from DaisyX.events import register
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
 db = client["missjuliarobot"]
 approved_users = db.approve
+
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
@@ -36,13 +36,17 @@ async def is_register_admin(chat, user):
         )
     return None
 
+
 configuration = cloudmersive_virus_api_client.Configuration()
-configuration.api_key['Apikey'] = VIRUS_API_KEY
-api_instance = cloudmersive_virus_api_client.ScanApi(cloudmersive_virus_api_client.ApiClient(configuration))
-allow_executables = True 
-allow_invalid_files = True 
-allow_scripts = True 
-allow_password_protected_files = True 
+configuration.api_key["Apikey"] = VIRUS_API_KEY
+api_instance = cloudmersive_virus_api_client.ScanApi(
+    cloudmersive_virus_api_client.ApiClient(configuration)
+)
+allow_executables = True
+allow_invalid_files = True
+allow_scripts = True
+allow_password_protected_files = True
+
 
 @register(pattern="^/scanit$")
 async def virusscan(event):
@@ -60,46 +64,52 @@ async def virusscan(event):
         else:
             return
     if not event.reply_to_msg_id:
-       await event.reply("Reply to a file to scan it.")
-       return
+        await event.reply("Reply to a file to scan it.")
+        return
 
     c = await event.get_reply_message()
     try:
-       c.media.document
+        c.media.document
     except Exception:
-       await event.reply("Thats not a file.")
-       return
+        await event.reply("Thats not a file.")
+        return
     h = c.media
     try:
-       k = h.document.attributes
+        k = h.document.attributes
     except Exception:
-       await event.reply("Thats not a file.")
-       return
+        await event.reply("Thats not a file.")
+        return
     if not isinstance(h, MessageMediaDocument):
-       await event.reply("Thats not a file.")
-       return
+        await event.reply("Thats not a file.")
+        return
     if not isinstance(k[0], DocumentAttributeFilename):
-       await event.reply("Thats not a file.")
-       return
+        await event.reply("Thats not a file.")
+        return
     try:
-      virus = c.file.name
-      await event.client.download_file(c, virus)
-      gg= await event.reply("Scanning the file ...")
-      fsize = c.file.size
-      if not fsize <= 3145700: # MAX = 3MB
-         await gg.edit("File size exceeds 3MB")
-         return
-      api_response = api_instance.scan_file_advanced(c.file.name, allow_executables=allow_executables, allow_invalid_files=allow_invalid_files, allow_scripts=allow_scripts, allow_password_protected_files=allow_password_protected_files)
-      if api_response.clean_result is True:
-       await gg.edit("This file is safe ✔️\nNo virus detected 🐞")
-      else:
-       await gg.edit("This file is Dangerous ☠️️\nVirus detected 🐞")
-      os.remove(virus)
+        virus = c.file.name
+        await event.client.download_file(c, virus)
+        gg = await event.reply("Scanning the file ...")
+        fsize = c.file.size
+        if not fsize <= 3145700:  # MAX = 3MB
+            await gg.edit("File size exceeds 3MB")
+            return
+        api_response = api_instance.scan_file_advanced(
+            c.file.name,
+            allow_executables=allow_executables,
+            allow_invalid_files=allow_invalid_files,
+            allow_scripts=allow_scripts,
+            allow_password_protected_files=allow_password_protected_files,
+        )
+        if api_response.clean_result is True:
+            await gg.edit("This file is safe ✔️\nNo virus detected 🐞")
+        else:
+            await gg.edit("This file is Dangerous ☠️️\nVirus detected 🐞")
+        os.remove(virus)
     except Exception as e:
-      print(e)
-      os.remove(virus)
-      await gg.edit("Some error occurred.")
-      return
+        print(e)
+        os.remove(virus)
+        await gg.edit("Some error occurred.")
+        return
 
 
 __help__ = """
