f41c5bd5d81cf5ad1986539f3f7c58bbbb6f12b2
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-06 13:33:40 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -203,7 +203,7 @@ updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
 pbot = Client("suzuyaPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
-
+tbot = telethn
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
