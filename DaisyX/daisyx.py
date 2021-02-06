257da9028e2e38ae1260d50598e9051ef79f2f98
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-06 13:31:54 +0530

    Update daisyx.py

--- DaisyX/daisyx.py
+++ DaisyX/daisyx.py
@@ -133,3 +133,45 @@ def juliabot(**args):
         return wrapper
 
     return decorator
+def chataction(**args):
+    """ Registers chat actions. """
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.ChatAction(**args))
+        return func
+
+    return decorator
+
+
+def userupdate(**args):
+    """ Registers user updates. """
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.UserUpdate(**args))
+        return func
+
+    return decorator
+
+
+def inlinequery(**args):
+    """ Registers inline query. """
+    pattern = args.get("pattern", None)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.InlineQuery(**args))
+        return func
+
+    return decorator
+
+
+def callbackquery(**args):
+    """ Registers inline query. """
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+
+    return decorator
