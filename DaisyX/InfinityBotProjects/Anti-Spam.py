77384810a8c988154e6b3c5ce3ca5e3a42fe23e7
InukaAsith <inukaasith7@gmail.com>
2021-02-07 02:15:30 +0000

    pylint: auto fixes

--- DaisyX/InfinityBotProjects/Anti-Spam.py
+++ DaisyX/InfinityBotProjects/Anti-Spam.py
@@ -1,18 +1,19 @@
-from DaisyX import BOT_ID
-import nude
-import html
 import asyncio
-from DaisyX.modules.sql import cleaner_sql as sql
+import html
+
+import better_profanity
+import nude
+from better_profanity import profanity
 from pymongo import MongoClient
-from DaisyX import MONGO_DB_URI
-from DaisyX.events import register
-from telethon import types, events
+from telethon import events, types
 from telethon.tl import *
 from telethon.tl.types import *
-from DaisyX import *
-import better_profanity
-from better_profanity import profanity
 from textblob import TextBlob
+
+from DaisyX import *
+from DaisyX import BOT_ID, MONGO_DB_URI
+from DaisyX.events import register
+from DaisyX.modules.sql import cleaner_sql as sql
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
@@ -195,7 +196,6 @@ async def _(event):
             return
         if str(event.sender_id) in str(userss) and str(event.chat_id) in str(iid):
             return
-        pass
     else:
         return
     if str(event.sender_id) == str(BOT_ID):
@@ -371,7 +371,6 @@ async def del_profanity(event):
     if event.is_group:
         if await is_register_admin(event.input_chat, event.message.sender_id):
             return
-        pass
     chats = spammers.find({})
     for c in chats:
         if event.text:
@@ -409,19 +408,18 @@ async def del_profanity(event):
         return
     msg = str(event.message)
     sender = await event.get_sender()
-    let = sender.username
+    sender.username
     if event.is_group:
         if await is_register_admin(event.input_chat, event.message.sender_id):
             return
-        pass
     chats = globalchat.find({})
     for c in chats:
         if event.text:
             if event.chat_id == c["id"]:
                 u = msg.split()
-                rm = " ".join(filter(lambda x:x[0]!='@', u))
+                rm = " ".join(filter(lambda x: x[0] != "@", u))
                 a = TextBlob(rm)
-                b = a.detect_language()    
+                b = a.detect_language()
                 if not b == "en":
                     await event.delete()
                     st = sender.first_name
