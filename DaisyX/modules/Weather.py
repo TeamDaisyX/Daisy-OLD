750376ba6138839f4df3fcb1cffcf7bc5815c3cb
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-07 22:28:49 +0530

    Create Weather.py

---
+++ DaisyX/modules/Weather.py
@@ -0,0 +1,7 @@
+__help__ = """
+ - /weather <city>: Get weather info in a particular place
+ - /wttr <city>: Advanced weather module, usage same as /weather
+ - /wttr moon: Get the current status of moon
+"""
+
+__mod_name__ = "Weather ⛈️"
