cee814628e435b0c881c1499695094ddeb864a95
InukaAsith <inukaasith7@gmail.com>
2021-02-12 09:53:33 +0000

    pylint: auto fixes

--- DaisyX/modules/tmpmail.py
+++ DaisyX/modules/tmpmail.py
@@ -1,26 +1,28 @@
-from tempmail import TempMail
 import time
-from DaisyX import *
-from DaisyX.events import register
+
 from pymongo import MongoClient
-from telethon import types, events
 from telethon.tl import *
 from telethon.tl.types import *
+from tempmail import TempMail
+
+from DaisyX import *
+from DaisyX.events import register
 
 client = MongoClient(MONGO_DB_URI)
 db = client["missjuliarobot"]
 tmail = db.tempmail
 
 tm = TempMail()
-api_host = 'privatix-temp-mail-v1.p.rapidapi.com'
-api_key=TEMP_MAIL_KEY
-tm.set_header(api_host,api_key)
+api_host = "privatix-temp-mail-v1.p.rapidapi.com"
+api_key = TEMP_MAIL_KEY
+tm.set_header(api_host, api_key)
+
 
 @register(pattern="^/registeremail$")
 async def _(event):
     if not event.is_private:
-       await event.reply("You can only use this service in PM!")
-       return
+        await event.reply("You can only use this service in PM!")
+        return
     gmail = tmail.find({})
     for c in gmail:
         if event.sender_id == c["user"]:
@@ -32,7 +34,10 @@ async def _(event):
     email = tm.get_email_address()
     hash = tm.get_hash(email)
     tmail.insert_one({"user": event.sender_id, "time": ttime, "email": hash})
-    await event.reply(f"You have successfully registered your account !\nYour temporary email is: {email}")
+    await event.reply(
+        f"You have successfully registered your account !\nYour temporary email is: {email}"
+    )
+
 
 __mod_name__ = "Temp Mail 📧"
 
