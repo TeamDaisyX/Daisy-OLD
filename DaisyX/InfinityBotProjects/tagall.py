09b22353cffade53b9cbcc516ee6600df82e695e
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-23 12:23:45 +0530

    Update tagall.py

--- DaisyX/InfinityBotProjects/tagall.py
+++ DaisyX/InfinityBotProjects/tagall.py
@@ -19,6 +19,29 @@ from telethon import events
 from DaisyX import telethn as tbot
 
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+
+
 @tbot.on(events.NewMessage(pattern="^/tagall (.*)"))
 async def _(event):
     if event.fwd_from:
