8ca8fffed4ef80a85c8b5eacf7f286be42c721a7
InukaAsith <inukaasith7@gmail.com>
2021-02-24 09:01:55 +0000

    pylint: auto fixes

--- DaisyX/InfinityBotProjects/shazam.py
+++ DaisyX/InfinityBotProjects/shazam.py
@@ -1,14 +1,13 @@
 import os
 
 import requests
-
-from DaisyX import telethn as borg
 from telethon import *
 from telethon import events
-from telethon.tl import functions, types
 from telethon.tl.types import *
 
 from DaisyX import *
+from DaisyX import telethn as borg
+
 
 async def fetch_audio(event, ws):
     if not event.reply_to_msg_id:
