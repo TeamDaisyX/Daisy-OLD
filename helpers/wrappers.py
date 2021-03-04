eaec21f338b382037415e38059fe94e9ecdab0ec
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-04 19:28:47 +0530

    Update wrappers.py

--- helpers/wrappers.py
+++ helpers/wrappers.py
@@ -3,13 +3,13 @@ from typing import Callable, Coroutine
 from pyrogram import Client
 from pyrogram.types import Message
 
-from DaisyX import SUDO_USERS
+from DaisyX import TIGERS
 from helpers.admins import get_administrators
 
 
 def admins_only(func: Callable) -> Coroutine:
     async def wrapper(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in TIGERS:
             return await func(client, message)
         admins = await get_administrators(message.chat)
         for admin in admins:
