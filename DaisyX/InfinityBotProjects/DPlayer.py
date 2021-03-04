30a4253a6c5c54915b83b9fd25b85a5a1c805dce
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-04 15:02:26 +0530

    Update DPlayer.py

--- DaisyX/InfinityBotProjects/DPlayer.py
+++ DaisyX/InfinityBotProjects/DPlayer.py
@@ -11,9 +11,7 @@ VOICE_CHATS = {}
 
 @pbot.on_message(filters.command(["play", "playmusic"]) & ~filters.private)
 async def test(client, message):
-    client.get_chat_member(message.chat.id, message.from_user.id)
-    if client.get_chat_member(chat_id, user_id).status in ("administrator", "creator"):
-        message.chat.id
+    if client.get_chat_member(message.chat.id).status in ("administrator", "creator"):
         message.chat.id
         if not message.reply_to_message and not message.reply_to_message.audio:
             await message.reply("`Reply To Audio To Play It`")
