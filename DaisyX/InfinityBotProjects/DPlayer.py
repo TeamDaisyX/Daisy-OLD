8178946a5f9212fbddb29c38881446305609286f
InukaAsith <inukaasith7@gmail.com>
2021-03-04 09:13:25 +0000

    pylint: auto fixes

--- DaisyX/InfinityBotProjects/DPlayer.py
+++ DaisyX/InfinityBotProjects/DPlayer.py
@@ -1,24 +1,19 @@
 import os
+
 import ffmpeg
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.raw.functions.channels import GetFullChannel
-from pyrogram.raw.functions.phone import LeaveGroupCall
-from pytgcalls import GroupCall
-import asyncio
-import math
-import time
 from pyrogram import filters
+from pytgcalls import GroupCall
+
 from DaisyX import pbot
 
-
-
 VOICE_CHATS = {}
+
+
 @pbot.on_message(filters.command(["play", "playmusic"]) & ~filters.private)
 async def test(client, message):
     user = client.get_chat_member(message.chat.id, message.from_user.id)
     if user.status is "administrator" or user.user.id in SUDO_USERS:
-        chat_id = message.chat.id
+        message.chat.id
         if not message.reply_to_message and not message.reply_to_message.audio:
             await message.reply("`Reply To Audio To Play It`")
             return
@@ -26,12 +21,11 @@ async def test(client, message):
         audio_original = await message.reply_to_message.download()
         await message.reply_text("`Please Wait, Let Me Download This File!`")
         ffmpeg.input(audio_original).output(
-        "stark.raw",
-        format='s16le',
-        acodec='pcm_s16le',
-        ac=2, ar='48k'
+            "stark.raw", format="s16le", acodec="pcm_s16le", ac=2, ar="48k"
         ).overwrite_output().run()
         group_call = GroupCall(client, "stark.raw")
         await group_call.start(message.chat.id)
-        await message.edit_text(f"DaisyX now Playing `{audio.title}...` in {message.chat.title}!")
+        await message.edit_text(
+            f"DaisyX now Playing `{audio.title}...` in {message.chat.title}!"
+        )
         os.remove(audio_original)
