de519bb4a32346f65f58053616cec4a0ca81d9e6
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-04 19:21:31 +0530

    Update DPlayer.py

--- DaisyX/InfinityBotProjects/DPlayer.py
+++ DaisyX/InfinityBotProjects/DPlayer.py
@@ -5,26 +5,30 @@ from pyrogram import filters
 from pytgcalls import GroupCall
 
 from DaisyX import pbot
+from cache.admins import set
+from helpers.wrappers import errors, admins_only
 
 VOICE_CHATS = {}
 
 
 @pbot.on_message(filters.command(["play", "playmusic"]) & ~filters.private)
+@admins_only
 async def test(client, message):
-    if client.get_chat_member(message.chat.id).status in ("administrator", "creator"):
-        message.chat.id
-        if not message.reply_to_message and not message.reply_to_message.audio:
-            await message.reply("`Reply To Audio To Play It`")
-            return
-        audio = message.reply_to_message.audio
-        audio_original = await message.reply_to_message.download()
-        await message.reply_text("`Please Wait, Let Me Download This File!`")
-        ffmpeg.input(audio_original).output(
-            "stark.raw", format="s16le", acodec="pcm_s16le", ac=2, ar="48k"
-        ).overwrite_output().run()
-        group_call = GroupCall(client, "stark.raw")
-        await group_call.start(message.chat.id)
-        await message.edit_text(
-            f"DaisyX now Playing `{audio.title}...` in {message.chat.title}!"
-        )
-        os.remove(audio_original)
+    if not message.reply_to_message and not message.reply_to_message.audio:
+        
+        await message.reply("`Reply To Audio To Play It`")
+        return
+    audio = message.reply_to_message.audio
+    audio_original = await message.reply_to_message.download()
+    await message.reply_text("`Please Wait, Let Me Download This File!`")
+    ffmpeg.input(audio_original).output(
+        "daisy.raw", format="s16le", acodec="pcm_s16le", ac=2, ar="48k"
+    ).overwrite_output().run()
+    group_call = GroupCall(client, "daisy.raw")
+    await group_call.start(message.chat.id)
+    await message.edit_text(
+        f"DaisyX now Playing `{audio.title}...` in {message.chat.title}!"
+    )
+    os.remove(audio_original)
+
+ 
