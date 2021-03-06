5657a1e7d8e23854ced31a75fc91ebc5d21b4c81
InukaAsith <inukaasith7@gmail.com>
2021-03-06 10:21:11 +0000

    pylint: auto fixes

--- DaisyX/Addons/handlers/callback.py
+++ DaisyX/Addons/handlers/callback.py
@@ -1,6 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
-from DaisyX import pbot as  Client
+
+from DaisyX import pbot as Client
+
 
 @Client.on_callback_query(filters.regex("close"))
 async def close(client: Client, query: CallbackQuery):
