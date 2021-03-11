a5e46d25671a47f514a66990a9e90de6d938264a
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-11 16:49:48 +0530

    Update Torrent.py

--- DaisyX/modules/Torrent.py
+++ DaisyX/modules/Torrent.py
@@ -386,3 +386,9 @@ async def paginate_nexttorrent(event):
             ],
         ],
     )
+__help__ = """
+ - /torrent <text>: Search for torrent links
+If you are still messed up send `/helptorrent` in pm for the tutorial !
+"""
+
+__mod_name__ = "Torrent"
