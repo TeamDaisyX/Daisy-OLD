f17d7ca4fed0c852f8641ec49f5287680548e13f
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-11 14:55:26 +0530

    Update Math.py

--- DaisyX/modules/Math.py
+++ DaisyX/modules/Math.py
@@ -1,32 +1,33 @@
-from DaisyX.decorator import register
-from .utils.disable import disableable_dec
-from .utils.message import get_arg
-from DaisyX.services.mongo import db
 import json
 import math
-
 import requests
 from telethon import types
 from telethon.tl import functions
+from DaisyX.decorator import register
+from .utils.disable import disableable_dec
+from .utils.message import get_arg
+#from DaisyX.services.mongo import db
+from .utils.message import get_args_str
+
 
 @register(cmds=['simplify', 'solve'])
 @disableable_dec('simplify')
-async def _(event):
-    args = event.pattern_match.group(1)
+async def _(message):
+    args = get_args_str(message)
     response = requests.get(f"https://newton.now.sh/api/v2/simplify/{args}")
     c = response.text
     obj = json.loads(c)
     j = obj["result"]
-    await event.reply(j)
+    await message.reply(j)
     
 @register(cmds=['factor', 'factorize'])
 @disableable_dec('factor')
-async def _(event):
-    args = event.pattern_match.group(1)
+async def _(message):
+    args = get_args_str(message)
     response = requests.get(f"https://newton.now.sh/api/v2/factor/{args}")
     c = response.text
     obj = json.loads(c)
     j = obj["result"]
-    await event.reply(j)
+    await message.reply(j)
 
     
