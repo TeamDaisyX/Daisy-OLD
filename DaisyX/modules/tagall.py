79aa37145bd31415db52cda88212c2aa4086b0dd
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-16 16:14:42 +0530

    Update tagall.py

--- DaisyX/modules/tagall.py
+++ DaisyX/modules/tagall.py
@@ -1,6 +1,6 @@
 from DaisyX.services.telethon import tbot as client
 from telethon import events
-import DaisyX.function.pluginhelpers as borg
+
 
 @client.on(events.NewMessage(pattern="/tagall(?: |$)(.*)"))
 async def _(event):
@@ -8,7 +8,7 @@ async def _(event):
         return
     mentions = "@tagall"
     chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
+    async for x in client.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
