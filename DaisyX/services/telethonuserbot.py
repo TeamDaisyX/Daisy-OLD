149da490b2bfbcbbe98671ec31e403df330a3d8d
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-16 16:27:08 +0530

    Create telethonuserbot.py

---
+++ DaisyX/services/telethonuserbot.py
@@ -0,0 +1,10 @@
+
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+try:
+    ubot.start()
+except BaseException:
+    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
