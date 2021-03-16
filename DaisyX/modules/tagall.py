437461adc443966483bfc35df8e17f94a9fb6d6f
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-16 16:23:29 +0530

    Update tagall.py

--- DaisyX/modules/tagall.py
+++ DaisyX/modules/tagall.py
@@ -1,14 +1,50 @@
-from DaisyX.services.telethon import tbot as client
+from DaisyX.services.telethon import tbot
 from telethon import events
+from DaisyX.services.mongo import mongodb as db
+
+approved_users = db.approve
 
 
-@client.on(events.NewMessage(pattern="/tagall(?: |$)(.*)"))
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return False
+
+@tbot.on(events.NewMessage(pattern="/listusers(?: |$)(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "@tagall"
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.sender_id):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
     chat = await event.get_input_chat()
-    async for x in client.iter_participants(chat, 100):
-        mentions += f"[\u2063](tg://user?id={x.id})"
-    await event.reply(mentions)
+    mentions = ""
+    sh = event.pattern_match.group(1) if event.pattern_match.group(1) else "Hi !"
+    async for x in event.client.iter_participants(chat):
+        mentions += f"[{x.first_name}](tg://user?id={x.id}) \n"
     await event.delete()
+    n = 4096
+    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
+    for i in kk:
+        j = f"**{sh}** \n{i}"
+        await event.client.send_message(event.chat_id, j)
