4101489e9569b5b2d6179c10042c977cee3053c6
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-16 15:41:02 +0530

    Update pluginhelpers.py

--- DaisyX/function/pluginhelpers.py
+++ DaisyX/function/pluginhelpers.py
@@ -17,8 +17,7 @@ import subprocess
 from typing import List, Optional, Tuple
 import time
 from datetime import datetime
-from telethon import events
-from DaisyX.services.telethon import tbot as borg
+
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
@@ -176,79 +175,3 @@ async def fetch_audio(client, message):
         final_warner = await message.reply_to_message.download(progress=progress, progress_args=(message, c_time, f'`Downloading Video!`'))
     await message.edit("`Almost Done!`")    
     return final_warner    
-
-
-# Thanks To Userge-X
-# Ported By @STARKXD
-async def convert_to_image(event, borg):
-    lmao = await event.get_reply_message()
-    if not (
-            lmao.gif
-            or lmao.audio
-            or lmao.voice
-            or lmao.video
-            or lmao.video_note
-            or lmao.photo
-            or lmao.sticker
-            or lmao.media
-    ):
-        await event.edit("`Format Not Supported.`")
-        return
-    else:
-        try:
-            c_time = time.time()
-            downloaded_file_name = await borg.download_media(
-                lmao.media,
-                sedpath,
-                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, event, c_time, "`Downloading...`")
-                ),
-            )
-        except Exception as e:  # pylint:disable=C0103,W0703
-            await event.edit(str(e))
-        else:
-            await event.edit(
-                "Downloaded to `{}` successfully.".format(downloaded_file_name)
-            )
-    if not os.path.exists(downloaded_file_name):
-        await event.edit("Download Unsucessfull :(")
-        return
-    if lmao and lmao.photo:
-        lmao_final = downloaded_file_name
-    elif lmao.sticker and lmao.sticker.mime_type == "application/x-tgsticker":
-        rpath = downloaded_file_name
-        image_name20 = os.path.join(sedpath, "SED.png")
-        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {downloaded_file_name} {image_name20}"
-        stdout, stderr = (await runcmd(cmd))[:2]
-        os.remove(rpath)
-        lmao_final = image_name20
-    elif lmao.sticker and lmao.sticker.mime_type == "image/webp":
-        pathofsticker2 = downloaded_file_name
-        image_new_path = sedpath + "image.png"
-        im = Image.open(pathofsticker2)
-        im.save(image_new_path, "PNG")
-        if not os.path.exists(image_new_path):
-            await event.edit("`Wasn't Able To Fetch Shot.`")
-            return
-        lmao_final = image_new_path
-    elif lmao.audio:
-        sed_p = downloaded_file_name
-        hmmyes = sedpath + "stark.mp3"
-        imgpath = sedpath + "starky.jpg"
-        os.rename(sed_p, hmmyes)
-        await runcmd(f"ffmpeg -i {hmmyes} -filter:v scale=500:500 -an {imgpath}")
-        os.remove(sed_p)
-        if not os.path.exists(imgpath):
-            await event.edit("`Wasn't Able To Fetch Shot.`")
-            return
-        lmao_final = imgpath
-    elif lmao.gif or lmao.video or lmao.video_note:
-        sed_p2 = downloaded_file_name
-        jpg_file = os.path.join(sedpath, "image.jpg")
-        await take_screen_shot(sed_p2, 0, jpg_file)
-        os.remove(sed_p2)
-        if not os.path.exists(jpg_file):
-            await event.edit("`Couldn't Fetch. SS`")
-            return
-        lmao_final = jpg_file
-    return lmao_final
