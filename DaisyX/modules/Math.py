e8ac27aefe9f066a2eb13d85456e1423e93ff8d4
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-16 10:43:10 +0530

    Update Math.py

--- DaisyX/modules/Math.py
+++ DaisyX/modules/Math.py
@@ -73,9 +73,9 @@ async def _(message):
     else:
         evaluation = "Sorry I can't find result for the given equation"
     final_output = "**EQUATION**: `{}` \n\n **SOLUTION**: \n`{}` \n".format(
-        cmd, evaluation
+        cmd, evaluation,.parse_mode = Markdown
     )
-    await message.reply(final_output)
+    await message.edit(final_output)
 
 
 async def aexec(code, event):
@@ -192,22 +192,22 @@ async def _(message):
     await message.reply(str(math.log(int(args))))        
 
 __help__ = """
-Solves complex math problems using https://newton.now.sh
- ✪ /simplify*:* Math `/math 2^2+2(2)`
- ✪ /factor*:* Factor `/factor x^2 + 2x`
- ✪ /derive*:* Derive `/derive x^2+2x`
- ✪ /integrate*:* Integrate `/integrate x^2+2x`
- ✪ /zeroes*:* Find 0's `/zeroes x^2+2x`
- ✪ /tangent*:* Find Tangent `/tangent 2lx^3`
- ✪ /area*:* Area Under Curve `/area 2:4lx^3`
- ✪ /cos*:* Cosine `/cos pi`
- ✪ /sin*:* Sine `/sin 0`
- ✪ /tan*:* Tangent `/tan 0`
- ✪ /arccos*:* Inverse Cosine `/arccos 1`
- ✪ /arcsin*:* Inverse Sine `/arcsin 0`
- ✪ /arctan*:* Inverse Tangent `/arctan 0`
- ✪ /abs*:* Absolute Value `/abs -1`
- ✪ /log*:* Logarithm `/log 2l8`
+Solves complex math problems using https://newton.now.sh and python math module
+ ✪ /simplify*: Math /math 2^2+2(2)
+ ✪ /factor*:* Factor /factor x^2 + 2x
+ ✪ /derive*:* Derive /derive x^2+2x
+ ✪ /integrate*:* Integrate /integrate x^2+2x
+ ✪ /zeroes*:* Find 0's /zeroes x^2+2x
+ ✪ /tangent*:* Find Tangent /tangent 2lx^
+ ✪ /area*:* Area Under Curve /area 2:4lx^3`
+ ✪ /cos*:* Cosine /cos pi
+ ✪ /sin*:* Sine /sin 0
+ ✪ /tan*:* Tangent /tan 0
+ ✪ /arccos*:* Inverse Cosine /arccos 1
+ ✪ /arcsin*:* Inverse Sine /arcsin 0
+ ✪ /arctan*:* Inverse Tangent /arctan 0
+ ✪ /abs*: Absolute Value /abs -1
+ ✪ /log*: Logarithm /log 2l8
 _Keep in mind_: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
 To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
 To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
