4fa57d14ad35a66dde287944447b8d9c50be8eef
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-17 13:03:43 +0530

    Update GroupGuardian.py

--- DaisyX/modules/GroupGuardian.py
+++ DaisyX/modules/GroupGuardian.py
@@ -275,6 +275,7 @@ async def del_profanity(event):
         chats = spammers.find({})
         for c in chats:
             if event.text:
+                
                 if event.chat_id == c["id"]:
                     if better_profanity.profanity.contains_profanity(msg):
                         await event.delete()
@@ -326,4 +327,93 @@ async def del_profanity(event):
                     await dev.delete()
     
 
-    
+@tbot.on(events.NewMessage(pattern=None))
+async def del_profanity(event):
+    if event.is_private:
+        return
+    msg = str(event.text)
+    sender = await event.get_sender()
+    let = sender.username
+    if await is_admin(event, event.message.sender_id):
+        return
+    chats = spammers.find({})
+    for c in chats:
+        if event.text:
+            if event.chat_id == c["id"]:
+                if better_profanity.profanity.contains_profanity(msg):
+                    await event.delete()
+                    if sender.username is None:
+                        st = sender.first_name
+                        hh = sender.id
+                        final = f"[{st}](tg://user?id={hh}) **{msg}** is detected as a slang word and your message has been deleted"
+                    else:
+                        final = f"@{let} **{msg}** is detected as a slang word and your message has been deleted"
+                    dev = await event.respond(final)
+                    await asyncio.sleep(10)
+                    await dev.delete()
+        if event.photo:
+            if event.chat_id == c["id"]:
+                await event.client.download_media(event.photo, "nudes.jpg")
+                if nude.is_nude("./nudes.jpg"):
+                    await event.delete()
+                    st = sender.first_name
+                    hh = sender.id
+                    final = f"[{st}](tg://user?id={hh}) you should only speak in english here !"
+                    dev = await event.respond(final)
+                    await asyncio.sleep(10)
+                    await dev.delete()
+                    os.remove("nudes.jpg")
+
+
+def extract_emojis(s):
+    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
+
+
+@tbot.on(events.NewMessage(pattern=None))
+async def del_profanity(event):
+    if event.is_private:
+        return
+    msg = str(event.message)
+    sender = await event.get_sender()
+    sender.username
+    if await is_admin(event, event.message.sender_id):
+        return
+    chats = globalchat.find({})
+    for c in chats:
+        if event.text:
+            if event.chat_id == c["id"]:
+                u = msg.split()
+                emj = extract_emojis(msg)
+                msg = msg.replace(emj, "")
+                if (
+                    [(k) for k in u if k.startswith("@")]
+                    and [(k) for k in u if k.startswith("#")]
+                    and [(k) for k in u if k.startswith("/")]
+                    and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+                ):
+                    h = " ".join(filter(lambda x: x[0] != "@", u))
+                    km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+                    tm = km.split()
+                    jm = " ".join(filter(lambda x: x[0] != "#", tm))
+                    hm = jm.split()
+                    rm = " ".join(filter(lambda x: x[0] != "/", hm))
+                elif [(k) for k in u if k.startswith("@")]:
+                    rm = " ".join(filter(lambda x: x[0] != "@", u))
+                elif [(k) for k in u if k.startswith("#")]:
+                    rm = " ".join(filter(lambda x: x[0] != "#", u))
+                elif [(k) for k in u if k.startswith("/")]:
+                    rm = " ".join(filter(lambda x: x[0] != "/", u))
+                elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+                    rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+                else:
+                    rm = msg
+                # print (rm)
+                b = translator.detect(rm)
+                if not "en" in b and not b == "":
+                    await event.delete()
+                    st = sender.first_name
+                    hh = sender.id
+                    final = f"[{st}](tg://user?id={hh}) you should only speak in english here !"
+                    dev = await event.respond(final)
+                    await asyncio.sleep(10)
+                    await dev.delete()   
