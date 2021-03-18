07eff48778ab9a7bf642b7d6b1f9f1b6287a8d68
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-18 11:11:49 +0530

    Update pyrogram.py

--- DaisyX/services/pyrogram.py
+++ DaisyX/services/pyrogram.py
@@ -19,8 +19,8 @@ from pyrogram import Client, errors
 from DaisyX.config import get_str_key, get_int_key
 
 TOKEN = get_str_key("TOKEN", required=True)
-APP_ID = get_int_key("APP_ID", required=True)
-APP_HASH = get_str_key("APP_HASH", required=True)
+APP_ID = get_int_key("APPID", required=True)
+APP_HASH = get_str_key("APPHASH", required=True)
 session_name = TOKEN.split(':')[0]
 pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
 
