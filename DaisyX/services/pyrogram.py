f0cd7a2bacb5ec11de5e3f1450d2bd47523f6beb
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-18 10:26:03 +0530

    Update pyrogram.py

--- DaisyX/services/pyrogram.py
+++ DaisyX/services/pyrogram.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import logging
 from pyrogram import Client, errors
 
 from DaisyX.config import get_str_key, get_int_key
@@ -21,6 +21,9 @@ from DaisyX.config import get_str_key, get_int_key
 TOKEN = get_str_key("TOKEN", required=True)
 APP_ID = get_int_key("APP_ID", required=True)
 APP_HASH = get_str_key("APP_HASH", required=True)
-pbot = Client("DaisyX", api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
+session_name = TOKEN.split(':')[0]
+pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
 
+# disable logging for pyrogram [not for ERROR logging]
+logging.getLogger('pyrogram').setLevel(level=logging.ERROR)
 pbot.start()
