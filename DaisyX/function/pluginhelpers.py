3f0cbdfd23af44455120018fb80f040868081399
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-19 15:33:51 +0530

    Update pluginhelpers.py

--- DaisyX/function/pluginhelpers.py
+++ DaisyX/function/pluginhelpers.py
@@ -2,7 +2,7 @@
 from math import ceil
 import textwrap
 from pyrogram import Client
-from pyrogram.types import InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent, Message
+from pyrogram.types import InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent, Message, User, Chat
 from pyrogram.errors import FloodWait, MessageNotModified
 import asyncio
 import shlex
@@ -14,11 +14,11 @@ import re
 import os
 import markdown
 import subprocess
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple,Callable, Coroutine, Dict, Union
 import time
 from datetime import datetime
 
-
+from DaisyX import OWNER_ID
 
 def get_user(message: Message, text: str) -> [int, str, None]:
     if text is None:
@@ -175,3 +175,49 @@ async def fetch_audio(client, message):
         final_warner = await message.reply_to_message.download(progress=progress, progress_args=(message, c_time, f'`Downloading Video!`'))
     await message.edit("`Almost Done!`")    
     return final_warner    
+
+# Admin check
+
+admins: Dict[str, List[User]] = {}
+
+
+def set(chat_id: Union[str, int], admins_: List[User]):
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
+    admins[chat_id] = admins_
+
+
+def get(chat_id: Union[str, int]) -> Union[List[User], bool]:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
+    if chat_id in admins:
+        return admins[chat_id]
+
+    return False
+
+
+async def get_administrators(chat: Chat) -> List[User]:
+    _get = get(chat.id)
+
+    if _get:
+        return _get
+    else:
+        set(
+            chat.id,
+            [member.user for member in await chat.get_members(filter="administrators")],
+        )
+        return await get_administrators(chat)
+
+
+def admins_only(func: Callable) -> Coroutine:
+    async def wrapper(client: Client, message: Message):
+        if message.from_user.id in OWNER_ID:
+            return await func(client, message)
+        admins = await get_administrators(message.chat)
+        for admin in admins:
+            if admin.id == message.from_user.id:
+                return await func(client, message)
+
+    return wrapper
