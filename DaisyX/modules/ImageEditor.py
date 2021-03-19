e5a2b2b06eb830ac70d14f96eb0ed91cf6674d13
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-19 16:14:28 +0530

    Update ImageEditor.py

--- DaisyX/modules/ImageEditor.py
+++ DaisyX/modules/ImageEditor.py
@@ -5,7 +5,6 @@
 from DaisyX.Addons.ImgEditor.edit_1 import (  # pylint:disable=import-error
     bright,
     mix,
-    black_white,
     g_blur,
     normal_blur,
     box_blur,
@@ -282,7 +281,43 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await mix(client, query.message)
 
         elif query.data == "b|w":
-            await black_white(client, query.message)
+            try:
+                userid = str(message.chat.id)
+                if not os.path.isdir(f"./DOWNLOADS/{userid}"):
+                    os.makedirs(f"./DOWNLOADS/{userid}")
+                download_location = "./DOWNLOADS" + "/" + userid + "/" + userid + ".jpg"
+                edit_img_loc = "./DOWNLOADS" + "/" + userid + "/" + "black_white.jpg"
+                if not message.reply_to_message.empty:
+                    msg = await message.reply_to_message.reply_text(
+                        "Downloading image", quote=True
+                    )
+                    a = await client.download_media(
+                        message=message.reply_to_message, file_name=download_location
+                    )
+                    await msg.edit("Processing Image...")
+                    image_file = cv2.imread(a)
+                    grayImage = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+                    cv2.imwrite(edit_img_loc, grayImage)
+                    await message.reply_chat_action("upload_photo")
+                    await message.reply_to_message.reply_photo(edit_img_loc, quote=True)
+                    await msg.delete()
+        else:
+            await message.reply_text("Why did you delete that??")
+        try:
+            shutil.rmtree(f"./DOWNLOADS/{userid}")
+        except Exception:
+            pass
+    except Exception as e:
+        print("black_white-error - " + str(e))
+        if "USER_IS_BLOCKED" in str(e):
+            return
+        else:
+            try:
+                await message.reply_to_message.reply_text(
+                    "Something went wrong!", quote=True
+                )
+            except Exception:
+                return
 
         elif query.data == "circlewithbg":
             await circle_with_bg(client, query.message)
