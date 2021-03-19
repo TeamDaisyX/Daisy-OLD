773dc3192fb15d5f84328094d1b206ddc6f985e7
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-19 17:20:02 +0530

    Update ImageEditor.py

--- DaisyX/modules/ImageEditor.py
+++ DaisyX/modules/ImageEditor.py
@@ -52,64 +52,64 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from DaisyX.services.pyrogram import pbot as Client 
 from pyrogram import filters
 
-@Client.on_message(filters.command(["edit", "editimage","editor"]))
-async def photo(client: Client, message: Message):
-    replied = message.reply_to_message
-    if not replied:
-        await edit_or_reply(message, text='Reply to a photo to open image editor')
-        return
-    if not replied.photo:
-        await edit_or_reply(message, text='Reply to a photo to open image editor')
-        return
-    try:
-        await client.send_message(
+@Client.on_message(filters.command(["imgeditor", "editimage","editor"]))
+async def hmm(client, message):
+    
+    await client.send_message(
             chat_id=message.chat.id,
-            text="Select your required mode from below!\n **Powered by @DaisySupport_Official**ㅤㅤ",
-            reply_markup=InlineKeyboardMarkup(
-                [
+            text="**Image Editor Running, Now send a image to process**ㅤㅤ")   
+    
+    @Client.on_message(filters.photo)
+    async def photo(client: Client, message: Message):
+        try:
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="Select your required mode from below!\n **Powered by @DaisySupport_Official**ㅤㅤ",
+                reply_markup=InlineKeyboardMarkup(
                     [
-                        InlineKeyboardButton(text="BRIGHT", callback_data="bright"),
-                        InlineKeyboardButton(text="MIXED", callback_data="mix"),
-                        InlineKeyboardButton(text="B&W", callback_data="b|w"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="CIRCLE", callback_data="circle"),
-                        InlineKeyboardButton(text="BLUR", callback_data="blur"),
-                        InlineKeyboardButton(text="BORDER", callback_data="border"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="STICKER", callback_data="stick"),
-                        InlineKeyboardButton(text="ROTATE", callback_data="rotate"),
-                        InlineKeyboardButton(text="CONTRAST", callback_data="contrast"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="SEPIA", callback_data="sepia"),
-                        InlineKeyboardButton(text="PENCIL", callback_data="pencil"),
-                        InlineKeyboardButton(text="CARTOON", callback_data="cartoon"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="INVERT", callback_data="inverted"),
-                        InlineKeyboardButton(text="GLITCH", callback_data="glitch"),
-                        InlineKeyboardButton(
-                            text="REMOVE BG", callback_data="removebg"
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(text="CLOSE", callback_data="close_e"),
-                    ],
-                ]
-            ),
-            reply_to_message_id = message.message_id,
-        )
-    except Exception as e:
-        print("photomarkup error - " + str(e))
-        if "USER_IS_BLOCKED" in str(e):
-            return
-        else:
-            try:
-                await message.reply_text("Something went wrong!", quote=True)
-            except Exception:
+                        [
+                            InlineKeyboardButton(text="BRIGHT", callback_data="bright"),
+                            InlineKeyboardButton(text="MIXED", callback_data="mix"),
+                            InlineKeyboardButton(text="B&W", callback_data="b|w"),
+                        ],
+                        [
+                            InlineKeyboardButton(text="CIRCLE", callback_data="circle"),
+                            InlineKeyboardButton(text="BLUR", callback_data="blur"),
+                            InlineKeyboardButton(text="BORDER", callback_data="border"),
+                        ],
+                        [
+                            InlineKeyboardButton(text="STICKER", callback_data="stick"),
+                            InlineKeyboardButton(text="ROTATE", callback_data="rotate"),
+                            InlineKeyboardButton(text="CONTRAST", callback_data="contrast"),
+                        ],
+                        [
+                            InlineKeyboardButton(text="SEPIA", callback_data="sepia"),
+                            InlineKeyboardButton(text="PENCIL", callback_data="pencil"),
+                            InlineKeyboardButton(text="CARTOON", callback_data="cartoon"),
+                        ],
+                        [
+                            InlineKeyboardButton(text="INVERT", callback_data="inverted"),
+                            InlineKeyboardButton(text="GLITCH", callback_data="glitch"),
+                            InlineKeyboardButton(
+                                text="REMOVE BG", callback_data="removebg"
+                            ),
+                        ],
+                        [
+                            InlineKeyboardButton(text="CLOSE", callback_data="close_e"),
+                        ],
+                    ]
+                ),
+                reply_to_message_id = message.message_id,
+            )
+        except Exception as e:
+            print("photomarkup error - " + str(e))
+            if "USER_IS_BLOCKED" in str(e):
                 return
+            else:
+                try:
+                    await message.reply_text("Something went wrong!", quote=True)
+                except Exception:
+                    return
 
 
 
