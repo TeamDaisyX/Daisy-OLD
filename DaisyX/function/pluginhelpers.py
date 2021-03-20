9e19d3df3cd1976d987e4052dd0fac887204fa30
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-20 09:58:08 +0530

    Update pluginhelpers.py

--- DaisyX/function/pluginhelpers.py
+++ DaisyX/function/pluginhelpers.py
@@ -176,6 +176,17 @@ async def fetch_audio(client, message):
     await message.reply("`Almost Done!`")    
     return final_warner    
 
+async def edit_or_reply(message, text, parse_mode="md"):
+    if message.from_user.id:
+        if message.reply_to_message:
+            kk = message.reply_to_message.message_id
+            return await message.reply_text(
+                text, reply_to_message_id=kk, parse_mode=parse_mode
+            )
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
+
+
 # Admin check
 
 admins: Dict[str, List[User]] = {}
