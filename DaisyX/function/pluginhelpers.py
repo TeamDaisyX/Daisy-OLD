146eb6d274add0db871885abde479261d957f84b
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-20 15:04:38 +0530

    Update pluginhelpers.py

--- DaisyX/function/pluginhelpers.py
+++ DaisyX/function/pluginhelpers.py
@@ -187,6 +187,19 @@ async def edit_or_reply(message, text, parse_mode="md"):
         return await message.reply_text(text, parse_mode=parse_mode)
     return await message.edit(text, parse_mode=parse_mode)
 
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
 
 # Admin check
 
