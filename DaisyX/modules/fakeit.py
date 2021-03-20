9a10767c4503288bfafd411094cd075a66f066ef
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-20 09:07:42 +0530

    Update fakeit.py

--- DaisyX/modules/fakeit.py
+++ DaisyX/modules/fakeit.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License	
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.	
 
-
+from DaisyX import BOT_ID
 from faker import Faker	
 from faker.providers import internet	
 from DaisyX.services.telethon import tbot
