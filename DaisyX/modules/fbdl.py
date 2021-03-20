62bc4eac543f585c27a386bc128e2ab4ad2b83ed
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-20 16:01:09 +0530

    Create fbdl.py

---
+++ DaisyX/modules/fbdl.py
@@ -0,0 +1,93 @@
+#    Copyright (C) @chsaiujwal 2020-2021
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from datetime import datetime
+import requests
+import re
+import html
+import os
+from DaisyX import BOT_ID
+from DaisyX.services.telethon import tbot
+from DaisyX.function.telethonbasics import is_admin
+from telethon import events
+
+daisythumb = "./DaisyX/stuff/daisy.jpg"
+
+def main(url, filename):
+    try:
+        download_video("HD", url, filename)
+    except(KeyboardInterrupt):
+        download_video("SD", url, filename)
+
+
+def download_video(quality, url, filename):
+    html = requests.get(url).content.decode('utf-8')
+    video_url = re.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
+    file_size_request = requests.get(video_url, stream=True)
+    file_size = int(file_size_request.headers['Content-Length'])
+    block_size = 1024
+    with open(filename + '.mp4', 'wb') as f:
+        for data in file_size_request.iter_content(block_size):
+            f.write(data)
+    print("\nVideo downloaded successfully.")
+
+
+
+@tbot.on(events.NewMessage(pattern="^/fbdl (.*)"))
+async def _(event):
+    if event.fwd_from:	
+        return	
+    if not await is_admin(event, BOT_ID): 
+        await event.reply("`I Should Be Admin To Do This!`")
+        return
+    if await is_admin(event, event.message.sender_id):  
+      url = event.pattern_match.group(1)
+      x = re.match(r'^(https:|)[/][/]www.([^/]+[.])*facebook.com', url)
+
+      if x:
+          html = requests.get(url).content.decode('utf-8')
+      else:
+          await event.reply("This Video Is Either Private Or URL Is Invalid. Exiting... ")
+          return
+
+      _qualityhd = re.search('hd_src:"https', html)
+      _qualitysd = re.search('sd_src:"https', html)
+      _hd = re.search('hd_src:null', html)
+      _sd = re.search('sd_src:null', html)
+
+      list = []
+      _thelist = [_qualityhd, _qualitysd, _hd, _sd]
+      for id,val in enumerate(_thelist):
+          if val != None:
+              list.append(id)
+      filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+
+      main(url, filename)
+      await event.reply("Video Downloaded Successfully. Starting To Upload.")
+
+      kk = f"{filename}.mp4"
+      caption= f"Facebook Video downloaded Successfully by @DaisyXBot.\nSay hi to devs @DaisySupport_Official."
+
+      await tbot.send_message(
+        event.chat_id,
+        caption,
+        thumb=daisythumb,
+        parse_mode="HTML",
+        file=kk,
+        force_document=True,
+        allow_cache=False,
+      )
+      os.system(f"rm {kk}")
+    else:
+        await event.reply("`You Should Be Admin To Do This!`")
+        return
