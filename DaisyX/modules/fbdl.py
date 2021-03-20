0ad7a75dd2ca73cd9399b159d3dc47f6654672f6
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-20 16:18:33 +0530

    Update fbdl.py

--- DaisyX/modules/fbdl.py
+++ DaisyX/modules/fbdl.py
@@ -1,4 +1,4 @@
-#    Copyright (C) @chsaiujwal 2020-2021
+#    Copyright (C) @chsaiujwal & InukaAsith 2020-2021
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
