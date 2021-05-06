65e9fa29f770f0a4ff9a69bdf25fa41850412182
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:06:55 +0530

    Update blacklist.py

--- DaisyX/modules/blacklist.py
+++ DaisyX/modules/blacklist.py
@@ -449,7 +449,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "Blacklists ⛔️"
+__mod_name__ = "Blacklists"
 
 __help__ = """
 Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
