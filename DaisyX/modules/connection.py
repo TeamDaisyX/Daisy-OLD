75ffe55e7fb85d61c1248c4c9d9f4dcfa801d823
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:11:35 +0530

    Update connection.py

--- DaisyX/modules/connection.py
+++ DaisyX/modules/connection.py
@@ -387,19 +387,19 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Connection 🌍"
+__mod_name__ = "Connection"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
 This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- ✪ /connect*:* Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
- ✪ /connection*:* List connected chats
- ✪ /disconnect*:* Disconnect from a chat
- ✪ /helpconnect*:* List available commands that can be used remotely
+ • /connect*:* Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
+ • /connection*:* List connected chats
+ • /disconnect*:* Disconnect from a chat
+ • /helpconnect*:* List available commands that can be used remotely
 
 *Admin only:*
- ✪ /allowconnect <yes/no>*:* allow a user to connect to a chat
+ • /allowconnect <yes/no>*:* allow a user to connect to a chat
 """
 
 CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, pass_args=True)
