67323214407ed2151f80c4e3446e442620a46be3
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:05:04 +0530

    Update backups.py

--- DaisyX/modules/backups.py
+++ DaisyX/modules/backups.py
@@ -372,15 +372,15 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups 📦"
+__mod_name__ = "Backups "
 
 __help__ = """
 *Only for group owner:*
 
- ✪ `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ • `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- ✪ `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ • `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
