abea7077be434c89ac34cf7c07eb12b8de7153cc
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:33:42 +0530

    Update stickers.py

--- DaisyX/modules/stickers.py
+++ DaisyX/modules/stickers.py
@@ -458,7 +458,7 @@ __help__ = """
 • `/stickers`*:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "Stickers 🎉"
+__mod_name__ = "Stickers"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
