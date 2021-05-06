847fe99fd6a5560d9684dc7fe9e68e3cffd1f207
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:16:19 +0530

    Update disable.py

--- DaisyX/modules/disable.py
+++ DaisyX/modules/disable.py
@@ -343,14 +343,14 @@ if is_module_loaded(FILENAME):
     ✪ /cmds*:* check the current status of disabled commands
 
     *Admins only:*
-    ✪ /enable <cmd name>*:* enable that command
-    ✪ /disable <cmd name>*:* disable that command
-    ✪ /enablemodule <module name>*:* enable all commands in that module
-    ✪ /disablemodule <module name>*:* disable all commands in that module
-    ✪ /listcmds*:* list all possible toggleable commands
+    • /enable <cmd name>*:* enable that command
+    • /disable <cmd name>*:* disable that command
+    • /enablemodule <module name>*:* enable all commands in that module
+    • /disablemodule <module name>*:* disable all commands in that module
+    • /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "Disabling ❎"
+    __mod_name__ = "Disabling"
 
 else:
     DisableAbleCommandHandler = CommandHandler
