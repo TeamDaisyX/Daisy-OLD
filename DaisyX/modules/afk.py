0ce5c379dc6b3ba3bc664050ed7d0cef87891a6b
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 07:59:18 +0530

    Update afk.py

--- DaisyX/modules/afk.py
+++ DaisyX/modules/afk.py
@@ -164,7 +164,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK 🏃‍♀️"
+__mod_name__ = "AFK "
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
