3339044d5242e31285d5b878de66edb24f8bccdf
MOAYAZ154 <81007472+MOAYAZ154@users.noreply.github.com>
2021-05-06 08:21:20 +0530

    Update locks.py

--- DaisyX/modules/locks.py
+++ DaisyX/modules/locks.py
@@ -576,7 +576,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks 🔐"
+__mod_name__ = "Locks"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
