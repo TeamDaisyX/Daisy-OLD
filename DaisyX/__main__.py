7f4bc0bfac999ce07ae4931c173537624d4387c8
sltechworld <85874927+sltechworld@users.noreply.github.com>
2021-06-26 10:37:27 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -361,7 +361,7 @@ def DaisyX_about_callback(update, context):
     if query.data == "aboutmanu_":
         query.message.edit_text(
             text=f"*😍 Hi again!  The name's {dispatcher.bot.first_name} 😍 \n\nAs  You I'm a next generational group management bot developed by Infinity_Bots.* "
-            f"\n\n 🔥 Join [SL Tech World](https://t.me/SL_Tech_Worldchat) To Keep Yourself Updated About {dispatcher.bot.first_name} 🔥"
+            f"\n\n 🔥 Join [SL_Tech_World](https://t.me/SL_Tech_Worldchat) To Keep Yourself Updated About {dispatcher.bot.first_name} 🔥"
             f"\n\n I have the normal GROUP MANAGING functions like flood control, a warning system etc but I mainly have the advanced and handy Antispam system and the SIBYL banning system which safegaurds and helps your group from spammers."
             f"\n\nI Can Manage Your Groups Smoothly, With Some Special Features [:)](https://telegra.ph/file/18db78aca96c01c79b27b.jpg)"
             f"\n\n👇 You Can Know More About Me By Clicking The Below Buttons 👇",
